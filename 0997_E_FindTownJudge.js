9de7b7d12c8534b992186da9be80f6d0e85a6c3f
Mark P Thomas <markums@gmail.com>
2022-02-26 18:00:02 -0700

    Updated names

---
+++ 0997_E_FindTownJudge.js
@@ -0,0 +1,26 @@
+// O() time complexity
+// O(1) space complexity
+// Time to complete: xx min
+// Patterns:
+// Notes w.r.t. solution:
+
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+ var findJudge = function(n, trust) {
+
+};
+
+// const testCases = [
+// { input: '',
+//   expected: ''},
+// ];
+
+// testCases.forEach((testCase) => {
+//   // let result = FUT(testCase.input); // insert function name here
+//   let pass = result === testCase.expected;
+//   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+//   }
+// );
---
+++ 1971_E_FindIfPathExistsInGraph.js
@@ -0,0 +1,29 @@
+// O() time complexity
+// O(1) space complexity
+// Time to complete: xx min
+// Patterns:
+// Notes w.r.t. solution:
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+ var validPath = function(n, edges, source, destination) {
+
+};
+
+// Test later
+// const testCases = [
+// { input: '',
+//   expected: ''},
+// ];
+
+// testCases.forEach((testCase) => {
+//   // let result = FUT(testCase.input); // insert function name here
+//   let pass = result === testCase.expected;
+//   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+//   }
+// );
