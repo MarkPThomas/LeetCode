10d3ee307e46b12b1d958ee942788a04d6e7b8de
Mark P Thomas <markums@gmail.com>
2022-02-27 13:03:09 -0700

    Completed problem

--- 1971_E_FindIfPathExistsInGraph.js
+++ 1971_E_FindIfPathExistsInGraph.js
@@ -1,7 +1,7 @@
 // O() time complexity
 // O(1) space complexity
 // Time to complete: xx min
-// Patterns:
+// Patterns: Graph
 // Notes w.r.t. solution:
 
 /**
@@ -12,8 +12,59 @@
  * @return {boolean}
  */
  var validPath = function(n, edges, source, destination) {
-
+  const graph = new Graph();
+  graph.buildGraph(edges, n);
+  return graph.hasPath(source, destination);
 };
+
+class Graph {
+  constructor() {
+      this.graph = {};
+  }
+
+  buildGraph(edges, numberOfEdges) {
+      // I don't like this. It presumes range of values < # of edges
+      // Also does not scale well if range of values is large if using edges.length
+      // See if hash map can replace
+      for (let i = 0; i < numberOfEdges; i++) {
+          this.graph[i] = [];
+      }
+
+      // Set up associations
+      for (let edge of edges) {
+          const [nodeI, nodeJ] = edge;
+
+          this.graph[nodeI].push(nodeJ);
+          this.graph[nodeJ].push(nodeI);
+      }
+  }
+
+  hasPath(source, destination) {
+      const visited = new Set();
+      const toVisit = [source];
+
+      while (toVisit.length) {
+          let currentNode = toVisit.shift(); // BFS
+          visited.add(currentNode);
+
+          if (currentNode === destination) {
+              return true;
+          } else {
+              // Add associations to stack if not visited
+              let associations = this.graph[currentNode];
+              for (let association of associations) {
+                  if (visited.has(association)) {
+                      continue;
+                  }
+
+                  toVisit.push(association);
+              }
+          }
+      }
+
+      return false;
+  }
+}
 
 // Test later
 // const testCases = [
