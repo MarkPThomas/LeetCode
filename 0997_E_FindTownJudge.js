b8b60e287b38a4010fee7d1f54b6d813f11b3214
Mark P Thomas <markums@gmail.com>
2022-02-28 00:16:42 -0700

    Completed

--- 0997_E_FindTownJudge.js
+++ 0997_E_FindTownJudge.js
@@ -10,8 +10,67 @@
  * @return {number}
  */
  var findJudge = function(n, trust) {
+  let townsPeople = new Graph();
+  townsPeople.buildGraph(n, trust);
 
+  return townsPeople.trustsNoOneButTrustedByAll();
 };
+
+class Graph {
+  constructor() {
+      this.graph = {};
+      this.numberOfPeople;
+  }
+
+  buildGraph(n, edges) {
+      this.numberOfPeople = n;
+
+      for (let i = 1; i <= n; i++) {
+          this.graph[i] = {};
+      }
+
+      for (let edge of edges) {
+          let [ truster, trusted ] = edge;
+          if (!this.graph[truster]) {
+              this.graph[truster] = {};
+          }
+          this.graph[truster][trusted] = true;
+      }
+  }
+
+  trustsNoOneButTrustedByAll() {
+      // we need to find who trusts no one, which means we need to visit all nodes to be sure
+      // depth first search, 2-way trust possible
+
+      let trustsNoOne = [];
+      for (let i = 1; i <= this.numberOfPeople; i++) {
+          let peopleTrustedByI = this.graph[i];
+          if (Object.keys(peopleTrustedByI).length === 0) {
+              trustsNoOne.push(i);
+          }
+      }
+
+      for (let i = 0; i < trustsNoOne.length; i++) {
+          let possibleJudge = trustsNoOne[i];
+          let trustedByAll = true;
+          for (let j = 1; j <= this.numberOfPeople; j++) {
+              // Skip self
+              if (j === possibleJudge) {
+                  continue;
+              }
+
+              if (!this.graph[j][possibleJudge]) {
+                  trustedByAll = false;
+              }
+          }
+          if (trustedByAll) {
+              return possibleJudge;
+          }
+      }
+
+      return -1;
+  }
+}
 
 // const testCases = [
 // { input: '',
