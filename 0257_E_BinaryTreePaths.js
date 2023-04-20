102e0c0b87db2f0bd5564d0e41a085c809e75919
Mark P Thomas <markums@gmail.com>
2023-04-20 11:12:14 -0600

    Completed test

--- 0257_E_BinaryTreePaths.js
+++ 0257_E_BinaryTreePaths.js
@@ -66,7 +66,7 @@ const testCases = [
 
 // =======
 testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
+  let result = binaryTreePaths(testCase.input);
   let pass = result === testCase.expected;
   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
 }
