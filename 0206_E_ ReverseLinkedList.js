882c784c8b444d4d60452bd96f145b5034f838ad
Mark P Thomas <markums@gmail.com>
2023-04-24 10:45:34 -0600

    Completed problem

---
+++ 0206_E_ ReverseLinkedList.js
@@ -0,0 +1,45 @@
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 14 min
+// Patterns: In-Place Reversal of Linked List
+// Notes w.r.t. solution: Remembered it, but was a bit rusty
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let currNode = head;
+  let nextNode = head?.next;
+  let prevNode = null;
+  while (currNode) {
+    currNode.next = prevNode;
+
+    prevNode = currNode;
+    currNode = nextNode;
+    nextNode = nextNode?.next;
+  }
+
+  return prevNode;
+};
+
+const testCases = [
+  {
+    input: '',
+    expected: ''
+  },
+];
+
+testCases.forEach((testCase) => {
+  // let result = FUT(testCase.input); // insert function name here
+  let pass = result === testCase.expected;
+  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+}
+);
