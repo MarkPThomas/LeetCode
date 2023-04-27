02303ef1db6628c2fcc4b987d4b55a5958a7d10d
Mark P Thomas <markums@gmail.com>
2023-04-27 12:27:30 -0700

    2023 retake of 2022 problem

--- 0020_E_ValidParentheses.js
+++ 0020_E_ValidParentheses.js
@@ -4,31 +4,63 @@
 // Patterns: stack structure
 // Notes w.r.t. solution:
 
+// 2022 Solution
 /**
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-  // FILO stack
-   let parentheses = {
-      '(': ')',
-      '[': ']',
-      '{': '}'
-  };
+var isValid = function (s) {
+    // FILO stack
+    let parentheses = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
 
-  let closeBrackets = [];
-  for (let i = 0; i < s.length; i++) {
-      // check if open bracket
-      if (parentheses[s[i]]) {
-          // add expected closing bracket to pop off
-          closeBrackets.push(parentheses[s[i]]);
-      } else {
-          // can only have a closing bracket that matches the top of the open brackets stack
-          if (closeBrackets.pop() !== s[i]) {
-              return false;
-          }
-      }
-  }
-  // All closing brackets must be emptied
-  return closeBrackets.length === 0;
+    let closeBrackets = [];
+    for (let i = 0; i < s.length; i++) {
+        // check if open bracket
+        if (parentheses[s[i]]) {
+            // add expected closing bracket to pop off
+            closeBrackets.push(parentheses[s[i]]);
+        } else {
+            // can only have a closing bracket that matches the top of the open brackets stack
+            if (closeBrackets.pop() !== s[i]) {
+                return false;
+            }
+        }
+    }
+    // All closing brackets must be emptied
+    return closeBrackets.length === 0;
+};
+
+// O(N) time complexity
+// O(N) space complexity
+// Time to complete: 10 min
+// Patterns: stack structure
+// Notes w.r.t. solution:
+
+// 2023 solution
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const validClosures = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+    const brackets = [];
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        const openBracket = brackets[brackets.length - 1];
+        if (brackets.length && validClosures[char] === openBracket) {
+            brackets.pop();
+        } else {
+            brackets.push(char);
+        }
+    }
+
+    return brackets.length === 0;
 };
