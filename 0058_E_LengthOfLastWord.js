b05c6fd5523e056d4175f2ec4a25e55651a89216
Mark P Thomas <markums@gmail.com>
2023-04-30 07:54:49 -0700

    Reworked problems for 2023

--- 0058_E_LengthOfLastWord.js
+++ 0058_E_LengthOfLastWord.js
@@ -1,3 +1,40 @@
+// 2023 Solution - w/ built-in functions
+// O(N) or less time complexity
+// O(1) space complexity
+// Time to complete:  1 min
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  return s.trim().split(' ').pop().length;
+};
+
+
+// 2023 Solution - Manual
+// O(N) or less time complexity
+// O(1) space complexity
+// Time to complete:  3 min
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let end = s.length - 1;
+  while (end >= 0 && s[end] === ' ') {
+    end--;
+  }
+
+  let start = end;
+  while (start >= 0 && s[start] !== ' ') {
+    start--;
+  }
+
+  return end - start;
+};
+
+
+// 2022 Solution
 // O(N) or less time complexity
 // O(1) space complexity
 // Time to complete:  4:40 min
@@ -6,40 +43,46 @@
  * @param {string} s
  * @return {number}
  */
- var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
   if (s.length < 1 || Math.pow(10, 4) < s.length) {
-      return 0;
+    return 0;
   }
 
   // Find last word
   let lastWord = [];
   for (let i = s.length - 1; i >= 0; i--) {
-      if (s[i] === ' ') {
-          if (lastWord.length === 0) {
-              continue;
-          } else {
-              break;
-          }
+    if (s[i] === ' ') {
+      if (lastWord.length === 0) {
+        continue;
       } else {
-          lastWord.push(s[i]);
+        break;
       }
+    } else {
+      lastWord.push(s[i]);
+    }
   }
 
   return lastWord.length;
 };
 
 const testCases = [
-{ input: 'Hello World',
-  expected: 5},
-{ input: '   fly me   to   the moon  ',
-  expected: 4},
-{ input: 'luffy is still joyboy',
-  expected: 6},
+  {
+    input: 'Hello World',
+    expected: 5
+  },
+  {
+    input: '   fly me   to   the moon  ',
+    expected: 4
+  },
+  {
+    input: 'luffy is still joyboy',
+    expected: 6
+  },
 ];
 
 testCases.forEach((testCase) => {
   let result = lengthOfLastWord(testCase.input);
   let pass = result === testCase.expected;
   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-  }
+}
 );
--- 0205_E_IsomorphicStrings.js
+++ 0205_E_IsomorphicStrings.js
@@ -1,3 +1,62 @@
+// 2023 Solution - Refactored
+// O(N) time complexity where N = s.length
+// O(1) space complexity
+// Time to complete: 5 min (including original solution)
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  function charsValidMap(keyChar, valChar, map) {
+    if (!map[keyChar]) {
+      map[keyChar] = valChar;
+    } else if (map[keyChar] !== valChar) {
+      return false;
+    }
+    return true;
+  }
+
+  const charMapST = {};
+  const charMapTS = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!charsValidMap(s[i], t[i], charMapST)
+      || !charsValidMap(t[i], s[i], charMapTS)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// 2023 Solution
+// O(N) time complexity where N = s.length
+// O(1) space complexity
+// Time to complete: 4 min
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const charMapST = {};
+  const charMapTS = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!charMapST[s[i]]) {
+      charMapST[s[i]] = t[i];
+    } else if (charMapST[s[i]] !== t[i]) {
+      return false;
+    }
+
+    if (!charMapTS[t[i]]) {
+      charMapTS[t[i]] = s[i];
+    } else if (charMapTS[t[i]] !== s[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// 2022 Solution
 // O(N) time complexity where N = s.length
 // O(1) space complexity
 // Time to complete: 19:07 min
@@ -7,60 +66,68 @@
  * @param {string} t
  * @return {boolean}
  */
- var isIsomorphic = function(s, t) {
+var isIsomorphic = function (s, t) {
   if (s.length !== t.length
-     || (s.length < 1 || Math.pow(50, 4) < s.length)) {
-      return false;
+    || (s.length < 1 || Math.pow(50, 4) < s.length)) {
+    return false;
   }
 
   let sToT = {};
   let tToS = {};
   for (let i = 0; i < s.length; i++) {
-      let charS = s[i];
-      let charT = t[i];
+    let charS = s[i];
+    let charT = t[i];
 
-      let charSMappedToT = sToT[charS];
-      let charTMappedToS = tToS[charT];
+    let charSMappedToT = sToT[charS];
+    let charTMappedToS = tToS[charT];
 
-      if (!charSMappedToT && !charTMappedToS) {
-          sToT[charS] = charT;
-          tToS[charT] = charS;
-      } else {
-          if (charSMappedToT !== charT
-             || charTMappedToS !== charS) {
-              return false;
-          }
+    if (!charSMappedToT && !charTMappedToS) {
+      sToT[charS] = charT;
+      tToS[charT] = charS;
+    } else {
+      if (charSMappedToT !== charT
+        || charTMappedToS !== charS) {
+        return false;
       }
+    }
   }
   return true;
 };
 
 const testCases = [
-  { input: {
+  {
+    input: {
       s: 'egg',
       t: 'add'
     },
-    expected: true},
-  { input: {
+    expected: true
+  },
+  {
+    input: {
       s: 'foo',
       t: 'bar'
     },
-    expected: false},
-  { input: {
+    expected: false
+  },
+  {
+    input: {
       s: 'paper',
       t: 'title'
     },
-    expected: true},
-  { input: {
+    expected: true
+  },
+  {
+    input: {
       s: 'abcde',
       t: 'abcda'
     },
-    expected: false},
+    expected: false
+  },
 ];
 
 testCases.forEach((testCase) => {
   let result = isIsomorphic(testCase.input.s, testCase.input.t);
   let pass = result === testCase.expected;
   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-  }
+}
 );
