4364a5713cb58e04e2da3d4acae9005db5c06c09
Mark P Thomas <markums@gmail.com>
2023-05-02 09:53:25 -0700

    Reworked problems for 2023

--- 0035_E_SearchInsertPosition.js
+++ 0035_E_SearchInsertPosition.js
@@ -1,3 +1,31 @@
+// O(log(n)) time complexity
+// O(1) space complexity
+// Time to complete: 4 min
+// Patterns: Binary search
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
+
 // O(log N) time complexity
 // O(1) space complexity
 // Time to complete: 26:55 min -> slow implementation of binary search, debugging for range error & by-1 error. Need to practice binary search!
@@ -9,41 +37,41 @@
  * @param {number} target
  * @return {number}
  */
- var searchInsert = function(nums, target) {
-  // return index of first slot where value >= target
-  // O(log n) -> binary search
-  if (target <= nums[0]) {
-      return 0;
-  }
-  if (target > nums[nums.length - 1]) {
-      return nums.length;
-  }
+var searchInsert = function (nums, target) {
+    // return index of first slot where value >= target
+    // O(log n) -> binary search
+    if (target <= nums[0]) {
+        return 0;
+    }
+    if (target > nums[nums.length - 1]) {
+        return nums.length;
+    }
 
-  let startIndex = 0;
-  let endIndex = nums.length - 1;
-  let midIndex = getMidIndex(startIndex, endIndex);
+    let startIndex = 0;
+    let endIndex = nums.length - 1;
+    let midIndex = getMidIndex(startIndex, endIndex);
 
-  while (indexDifference(startIndex, endIndex) > 1) {
-      let currentNum = nums[midIndex];
-      if (currentNum === target) {
-          return midIndex;
-      } else if (currentNum < target) {
-          startIndex = midIndex + 1
-      } else { // currentNum > target
-          endIndex = midIndex;
-      }
-      midIndex = getMidIndex(startIndex, endIndex);
-  }
+    while (indexDifference(startIndex, endIndex) > 1) {
+        let currentNum = nums[midIndex];
+        if (currentNum === target) {
+            return midIndex;
+        } else if (currentNum < target) {
+            startIndex = midIndex + 1
+        } else { // currentNum > target
+            endIndex = midIndex;
+        }
+        midIndex = getMidIndex(startIndex, endIndex);
+    }
 
-  return nums[midIndex] < target ? midIndex + 1 : midIndex
+    return nums[midIndex] < target ? midIndex + 1 : midIndex
 };
 
-const getMidIndex = function(startIndex, endIndex) {
-  return startIndex + Math.floor((endIndex - startIndex) / 2);
+const getMidIndex = function (startIndex, endIndex) {
+    return startIndex + Math.floor((endIndex - startIndex) / 2);
 }
 
-const indexDifference = function(startIndex, endIndex) {
-  return endIndex - startIndex;
+const indexDifference = function (startIndex, endIndex) {
+    return endIndex - startIndex;
 }
 
 // TODO: Add tests later
--- 0203_E_RemoveLinkedListElements.js
+++ 0203_E_RemoveLinkedListElements.js
@@ -1,5 +1,41 @@
 // O(N) time complexity
 // O(1) space complexity
+// Time to complete: 7 min
+// Patterns:
+// Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    if (!head) {
+        return head;
+    }
+    let prevNode = head;
+    let currNode = head.next;
+    while (currNode) {
+        if (currNode.val === val) {
+            prevNode.next = currNode.next;
+            currNode = currNode.next;
+        } else {
+            prevNode = prevNode.next;
+            currNode = currNode.next;
+        }
+    }
+    return head.val === val ? head.next : head;
+};
+
+
+// O(N) time complexity
+// O(1) space complexity
 // Time to complete: 12:20 min (8:30 min but had to debug)
 // Patterns: Sentinel node (not used here, but see solution: https://leetcode.com/problems/remove-linked-list-elements/solution/)
 // Notes w.r.t. solution: I did not use a sentinel node but still have the same time-space complexity
@@ -16,22 +52,22 @@
  * @param {number} val
  * @return {ListNode}
  */
- var removeElements = function(head, val) {
-  let node = head;
+var removeElements = function (head, val) {
+    let node = head;
 
-  while (node) {
-      // Reset node link to next node that does not match val, stopping at the end
-      while (node.next && node.next.val === val) {
-          node.next = node.next.next;
-      }
-      node = node.next;
-  }
+    while (node) {
+        // Reset node link to next node that does not match val, stopping at the end
+        while (node.next && node.next.val === val) {
+            node.next = node.next.next;
+        }
+        node = node.next;
+    }
 
-  // Check head
-  if (head && head.val === val) {
-      head = head.next;
-  }
-  return head;
+    // Check head
+    if (head && head.val === val) {
+        head = head.next;
+    }
+    return head;
 };
 
 // Test later
