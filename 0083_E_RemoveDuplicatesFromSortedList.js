89ffb64462eb0f0dafeb00db41988d4f213675fc
Mark P Thomas <markums@gmail.com>
2023-05-03 12:19:43 -0600

    Redid problem for 2023

--- 0083_E_RemoveDuplicatesFromSortedList.js
+++ 0083_E_RemoveDuplicatesFromSortedList.js
@@ -1,7 +1,47 @@
+// 2023
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 3 min
+// Patterns: Linked List
+// Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head) {
+        return head;
+    }
+
+    let prevNode = head;
+    let nextNode = head.next;
+    while (nextNode) {
+        if (prevNode.val === nextNode.val) {
+            prevNode.next = nextNode.next;
+        } else {
+            prevNode = prevNode.next;
+        }
+        nextNode = nextNode.next;
+    }
+    if (prevNode.val === head.val) {
+        head.next = null;
+    }
+
+    return head;
+};
+
+// 2022
 // O(N) time complexity
 // O(1) space complexity
 // Time to complete: 12:57 min (basically finished in 6:00 but got tripped up on minor linked list call)
-// Patterns: Lin
+// Patterns: Linked Lists
 // Notes w.r.t. solution:
 
 /**
@@ -15,28 +55,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var deleteDuplicates = function(head) {
-  let currentNode = head;
-  while (currentNode && currentNode.next) {
-      if (currentNode.val === currentNode.next.val) {
-          currentNode.next = currentNode.next.next;
-      } else {
-          currentNode = currentNode.next;
-      }
-  }
+var deleteDuplicates = function (head) {
+    let currentNode = head;
+    while (currentNode && currentNode.next) {
+        if (currentNode.val === currentNode.next.val) {
+            currentNode.next = currentNode.next.next;
+        } else {
+            currentNode = currentNode.next;
+        }
+    }
 
-  return head;
+    return head;
 };
-
-// Test later
-// const testCases = [
-// { input: '',
-//   expected: ''},
-// ];
-
-// testCases.forEach((testCase) => {
-//   // let result = FUT(testCase.input); // insert function name here
-//   let pass = result === testCase.expected;
-//   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-//   }
-// );
--- 0088_E_MergeSortedArray.js
+++ 0088_E_MergeSortedArray.js
@@ -1,5 +1,38 @@
+// 2023
+// O(n + m) time complexity
+// O(1) space complexity
+// where n = length of nums 1, m = length of nums 2
+// Time to complete: 25 min (went into 2 wrong solutions at first. Should have thought it out more carefully)
+// Patterns: 3 pointers, start from end
+// Notes w.r.t. solution:
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void}
+ */
+var merge = function (nums1, m, nums2, n) {
+    let ptr1 = m - 1;
+    let ptr2 = n - 1;
+    for (let i = m + n - 1; i >= 0; i--) {
+        if (ptr2 < 0) { // optimization
+            break;
+        }
+        if (ptr1 >= 0 && nums1[ptr1] >= nums2[ptr2]) {
+            nums1[i] = nums1[ptr1];
+            ptr1--;
+        } else {
+            nums1[i] = nums2[ptr2];
+            ptr2--;
+        }
+    }
+};
+
+// 2022
 // O(m+n) time complexity
 // O(1) space complexity
+// where n = length of nums 1, m = length of nums 2
 // Time to complete: 16 min for main solve, 20:40 for total (a bit too fast again, 99% there, debugging)
 // Patterns: Multiple pointers, working from ends forwards
 // Notes w.r.t. solution: Got the medium-level solution to the easy problem!
@@ -11,63 +44,45 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-  // m states the point where nums1 is 0
-  // ultimately nums1 has m+n, so last entry is the least of nums1[m] & nums2[n]
-  // makes sense to have 3 pointers:
-  //      1. num1 @ m, working forwards
-  //      2. num2 @ n, working forwards
-  //      3. num1 @ end, working forwards
-  if (n === 0) {
-      return;
-  }
+var merge = function (nums1, m, nums2, n) {
+    // m states the point where nums1 is 0
+    // ultimately nums1 has m+n, so last entry is the least of nums1[m] & nums2[n]
+    // makes sense to have 3 pointers:
+    //      1. num1 @ m, working forwards
+    //      2. num2 @ n, working forwards
+    //      3. num1 @ end, working forwards
+    if (n === 0) {
+        return;
+    }
 
-  if (m + n === 0) {
-      nums1 = nums2;
-      return;
-  }
+    if (m + n === 0) {
+        nums1 = nums2;
+        return;
+    }
 
-  let num1Index = m - 1;
-  let num2Index = n - 1;
-  let insertIndex = m + n - 1;
+    let num1Index = m - 1;
+    let num2Index = n - 1;
+    let insertIndex = m + n - 1;
 
-  while (insertIndex >= 0 && num1Index >= 0 && num2Index >= 0) { // O(m+n)
-      let value1 = nums1[num1Index];
-      let value2 = nums2[num2Index];
+    while (insertIndex >= 0 && num1Index >= 0 && num2Index >= 0) { // O(m+n)
+        let value1 = nums1[num1Index];
+        let value2 = nums2[num2Index];
 
-      if (value1 < value2) {
-          nums1[insertIndex] = value2;
-          num2Index--;
-      } else {
-          nums1[insertIndex] = value1;
-          num1Index--;
-      }
-      insertIndex--;
-  }
-  // if insertIndex reaches beginning, do nothing
-  // if nums2 is emptied first, do nothing
-  // if nums1 is emptied first, place remaining nums2 in nums1 in order
-  while (num2Index >= 0) {  // O(n)
-      nums1[insertIndex] = nums2[num2Index];
-      num2Index--;
-      insertIndex--;
-  }
+        if (value1 < value2) {
+            nums1[insertIndex] = value2;
+            num2Index--;
+        } else {
+            nums1[insertIndex] = value1;
+            num1Index--;
+        }
+        insertIndex--;
+    }
+    // if insertIndex reaches beginning, do nothing
+    // if nums2 is emptied first, do nothing
+    // if nums1 is emptied first, place remaining nums2 in nums1 in order
+    while (num2Index >= 0) {  // O(n)
+        nums1[insertIndex] = nums2[num2Index];
+        num2Index--;
+        insertIndex--;
+    }
 };
-
-// test later
-// const testCases = [
-// { input: {
-//   nums1: [],
-//   m: 0,
-//   nums2: [],
-//   n: 0
-//   },
-//   expected: ''},
-// ];
-
-// testCases.forEach((testCase) => {
-//   let result = merge(testCase.input.nums1, testCase.input.m, testCase.input.nums2, testCase.input.n);
-//   let pass = result === testCase.expected;
-//   console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-//   }
-// );
