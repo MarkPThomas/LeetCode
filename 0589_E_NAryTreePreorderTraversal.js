ca9b92a31252a4f44080b88e4ba2617b3def591c
Mark P Thomas <markums@gmail.com>
2023-05-06 21:10:46 -0600

    Completed problem

--- 0589_E_NAryTreePreorderTraversal.js
+++ 0589_E_NAryTreePreorderTraversal.js
@@ -1,17 +1,64 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Iteration strategy
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 2 min
+// Patterns: DFS Preorder Traversal w/ iteration
 // Notes w.r.t. solution:
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  if (!root) {
+    return [];
   }
-);
+  const values = [];
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    values.push(node.val);
+    queue.unshift(...node.children);
+  }
+  return values;
+};
+
+
+// Recursion strategy
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 3 min
+// Patterns: DFS Preorder Traversal w/ Recursion
+// Notes w.r.t. solution:
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  const values = [];
+  return preOrderRecursive(root, values);
+};
+
+function preOrderRecursive(root, values) {
+  if (root === null) {
+    return values;
+  }
+  values.push(root.val);
+  root.children.forEach((child) => values = preOrderRecursive(child, values));
+  return values;
+}
