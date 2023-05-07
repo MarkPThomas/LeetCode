ca9d7f0ae5465e308030cc83f4139e447e2d70f8
Mark P Thomas <markums@gmail.com>
2023-05-07 13:22:24 -0600

    Completed problem

--- 0605_E_CanPlaceFlowers.js
+++ 0605_E_CanPlaceFlowers.js
@@ -1,17 +1,116 @@
-// O() time complexity
+// Optimized & simplified solution
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
+// Time to complete: 21:00
+// Patterns: Greedy?
+// Notes w.r.t. solution: Worked out MUCH better by more carefully working out cases carefully
+//  for both when to count a plot as valid, and how much to increment (optimization)
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  if (n === 0
+    || (n === 1 && flowerbed.length === 1 && flowerbed[0] === 0)) {
+    return true;
+  }
+
+  let i = 0;
+  while (i < flowerbed.length) {
+    if (i === 0
+      && flowerbed[0] === 0
+      && flowerbed[1] === 0) {
+      // Left edge of flowerbed
+      n--;
+    } else if (i > flowerbed.length - 3
+      && flowerbed[flowerbed.length - 1] === 0
+      && flowerbed[flowerbed.length - 2] === 0) {
+      // Right edge of flowerbed
+      n--;
+    } else if (
+      flowerbed[i - 1] === 0
+      && flowerbed[i] === 0
+      && flowerbed[i + 1] === 0) {
+      // Inside flowerbed
+      n--;
+    }
+
+    if (i > 0 && flowerbed[i - 1] === 1) {
+      // last plot is filled
+      i += 1;
+    } else if (i < flowerbed.length - 1 && flowerbed[i + 1] === 1) {
+      // next plot is filled
+      i += 3;
+    } else {
+      i += 2;
+    }
+
+    if (n === 0) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Original solution
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 1:14:00 min (ouch!)
 // Patterns:
 // Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  if (n === 0
+    || (n === 1 && flowerbed.length === 1 && flowerbed[0] === 0)) {
+    return true;
   }
-);
+
+  // Handle case of [{1, 0,} 1] for n = 1, multiples of 2
+  let emptySpacesNeeded = 2 * n;
+
+  let emptyCount = 0;
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0) {
+      emptyCount++;
+      if (emptySpacesNeeded === 1) {
+        return true;
+      }
+    } else {
+      emptyCount = 0;
+      // Handle case of [1, {0, 1, 0,} 1] for n = 1, multiples of 2
+      if (emptySpacesNeeded % 2 === 0) {
+        emptySpacesNeeded += 1;
+      }
+    }
+
+    if (emptyCount > 1 && emptyCount % 2 === 0 && emptySpacesNeeded % 2 === 0) {
+      // case adjacent to beginning of bed, before any filled space is encountered
+      emptySpacesNeeded -= 2;
+      emptyCount = 0;
+    } else if (emptyCount > 2 && emptyCount % 2 !== 0) {
+      // cases after first filled space encountered
+      if (emptySpacesNeeded === 3) {
+        // last empty space
+        return true;
+      } else {
+        // intermediate empty space
+        emptySpacesNeeded -= 2;
+        emptyCount -= 2;
+      }
+    } else if (i === flowerbed.length - 1 && emptyCount === emptySpacesNeeded - 1) {
+      // last empty space, at end of flower bed
+      return true;
+    }
+
+    if (emptySpacesNeeded === 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
