f34176e78e66d494d1255ea03e89592f0c937c49
Mark P Thomas <markums@gmail.com>
2023-05-07 11:30:00 -0600

    Reworked 2022 problems

--- 0013_E_RomanToInteger.js
+++ 0013_E_RomanToInteger.js
@@ -1,3 +1,91 @@
+// 2023 Improved
+// O(n) -> O(1) time complexity as there is a finite set of Roman Numerals
+// O(1) space complexity
+// Time to complete: 7 min
+// Patterns: Hash Map, 2 ptrs
+// Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const numeralValues = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    if (s.length === 1) {
+        return numeralValues[s[0]];
+    }
+    // work right-left, adding count
+    // look one ahead & if value is less, subtract it & skip it
+    let lastVal = 0;
+    let total = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        const val = numeralValues[s[i]];
+        if (lastVal > val) {
+            total -= val;
+        } else {
+            total += val;
+        }
+        lastVal = val;
+    }
+
+    return total;
+};
+
+// 2023 Original
+// O(n) -> O(1) time complexity as there is a finite set of Roman Numerals
+// O(1) space complexity
+// Time to complete: 7 min
+// Patterns: Hash Map, 2 ptrs
+// Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const numeralValues = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    if (s.length === 1) {
+        return numeralValues[s[0]];
+    }
+    // work right-left, adding count
+    // look one ahead & if value is less, subtract it & skip it
+    let ptrVal = s.length - 1;
+    let ptrSub = s.length - 2;
+    let value = 0;
+    while (ptrVal >= 0) {
+        const val = numeralValues[s[ptrVal]];
+        value += val;
+        if (ptrSub >= 0) {
+            const valSub = numeralValues[s[ptrSub]];
+            if (valSub < val) {
+                value -= valSub;
+                ptrVal--;
+                ptrSub--;
+            }
+        }
+        ptrVal--;
+        ptrSub--;
+    }
+    return value;
+};
+
+// 2022
 // O(N) time complexity
 // O(1) space complexity
 // Time to complete: N/A
@@ -6,72 +94,78 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-  // Validate string length
-  if (s.length < 1 || 15 < s.length) {
-      return 0;
-  }
+var romanToInt = function (s) {
+    // Validate string length
+    if (s.length < 1 || 15 < s.length) {
+        return 0;
+    }
 
-  const numerals = {
-      I: 1,
-      V: 5,
-      X: 10,
-      L: 50,
-      C: 100,
-      D: 500,
-      M: 1000
-  }
+    const numerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
 
-  const numeralSubtractions = {
-      V : 'I',
-      X: 'I',
-      L: 'X',
-      C: 'X',
-      D: 'C',
-      M: 'C'
-  }
+    const numeralSubtractions = {
+        V: 'I',
+        X: 'I',
+        L: 'X',
+        C: 'X',
+        D: 'C',
+        M: 'C'
+    }
 
-  let count = 0;
-  let priorChar = '';
+    let count = 0;
+    let priorChar = '';
 
-  for (let i = s.length - 1; i >= 0; i--) {
-      let currentChar = s[i];
-      let charCount = numerals[currentChar];
+    for (let i = s.length - 1; i >= 0; i--) {
+        let currentChar = s[i];
+        let charCount = numerals[currentChar];
 
-      // Enforce character is valid
-      if (!charCount) {
-          return 0;
-      }
+        // Enforce character is valid
+        if (!charCount) {
+            return 0;
+        }
 
-      if (priorChar) {
-          const numeralSubtraction = numeralSubtractions[priorChar];
-          if (currentChar === numeralSubtraction) {
-            charCount = -numerals[numeralSubtraction];
-            if (!charCount) {
-                return 0;
+        if (priorChar) {
+            const numeralSubtraction = numeralSubtractions[priorChar];
+            if (currentChar === numeralSubtraction) {
+                charCount = -numerals[numeralSubtraction];
+                if (!charCount) {
+                    return 0;
+                }
             }
-          }
-      }
+        }
 
-      count += charCount
-      priorChar = currentChar;
-  }
+        count += charCount
+        priorChar = currentChar;
+    }
 
-  return count;
+    return count;
 };
 
 const testCases = [
-{ input: 'III',
- expected: 3},
-{ input: 'LVIII',
- expected: 58},
-{ input: 'MCMXCIV',
- expected: 1994},
+    {
+        input: 'III',
+        expected: 3
+    },
+    {
+        input: 'LVIII',
+        expected: 58
+    },
+    {
+        input: 'MCMXCIV',
+        expected: 1994
+    },
 ];
 
 testCases.forEach((testCase) => {
-  let result = romanToInt(testCase.input);
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}, Expected: ${testCase.expected}, Result: ${result}, Pass: ${pass}`);
-  }
+    let result = romanToInt(testCase.input);
+    let pass = result === testCase.expected;
+    console.log(`Input: ${testCase.input}, Expected: ${testCase.expected}, Result: ${result}, Pass: ${pass}`);
+}
 );
--- 0141_E_LinkedListCycle.js
+++ 0141_E_LinkedListCycle.js
@@ -1,3 +1,40 @@
+// 2023
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 10 min
+// Patterns: Linked list, 2 runners
+// Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (!head) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+    do {
+        if (fast.next === null || fast.next.next === null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    } while (slow !== fast)
+
+    return true;
+};
+
+
+// 2022
 // O(n) time complexity
 // O(1) space complexity
 // Time to complete: 8:09 min
@@ -16,21 +53,21 @@
  * @param {ListNode} head
  * @return {boolean}
  */
- var hasCycle = function(head) {
-  let ptrSlow = head;
-  let ptrFast = head;
+var hasCycle = function (head) {
+    let ptrSlow = head;
+    let ptrFast = head;
 
-  while (ptrSlow !== null && ptrFast !== null) {
-      ptrSlow = ptrSlow.next;
-      ptrFast = ptrFast.next;
-      if (ptrFast === null) {
-          break;
-      } else {
-         ptrFast = ptrFast.next;
-      }
-      if (ptrSlow === ptrFast) {
-          return true;
-      }
-  }
-  return false;
+    while (ptrSlow !== null && ptrFast !== null) {
+        ptrSlow = ptrSlow.next;
+        ptrFast = ptrFast.next;
+        if (ptrFast === null) {
+            break;
+        } else {
+            ptrFast = ptrFast.next;
+        }
+        if (ptrSlow === ptrFast) {
+            return true;
+        }
+    }
+    return false;
 };
