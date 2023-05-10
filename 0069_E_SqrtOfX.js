0c310a0f0e7e33c8dc48f54fd900410914065a67
Mark P Thomas <markums@gmail.com>
2023-05-10 00:18:20 -0600

    Redid problem for 2023

--- 0069_E_SqrtOfX.js
+++ 0069_E_SqrtOfX.js
@@ -1,3 +1,36 @@
+// 2023
+// O(log(n)) time complexity
+// O(1) space complexity
+// Time to complete: 7 min
+// Patterns: Binary search
+// Notes w.r.t. solution:
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    // Find first # that <= x when squared,
+    // such that the next highest # > x when squared
+    let left = 0;
+    let right = x;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const square = mid * mid;
+        if (square === x) {
+            return mid;
+        }
+        if (square < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left - 1;
+};
+
+
+// 2022
 // O(log(N)) time complexity
 // O(1) space complexity
 // Time to complete: 23:20 min
@@ -8,28 +41,28 @@
  * @param {number} x
  * @return {number}
  */
- var mySqrt = function(x) {
-  if (x < 2) {
-      return x;
-  }
+var mySqrt = function (x) {
+    if (x < 2) {
+        return x;
+    }
 
-  let getPivot = x => Math.floor(x / 2);
-  let sqrt = getPivot(x);
-  let lastSqrt = x;
-  let squared = sqrt * sqrt;
-  while (squared !== x && lastSqrt !== sqrt) {
-      if (squared < x) {
-          // increase unless within 1 int
-          if (lastSqrt - sqrt < 2) {
-              break;
-          }
-          sqrt += getPivot(lastSqrt - sqrt);
-      } else {
-          // decrease
-          lastSqrt = sqrt;
-          sqrt = getPivot(sqrt);
-      }
-      squared = sqrt * sqrt;
-  }
-  return sqrt;
+    let getPivot = x => Math.floor(x / 2);
+    let sqrt = getPivot(x);
+    let lastSqrt = x;
+    let squared = sqrt * sqrt;
+    while (squared !== x && lastSqrt !== sqrt) {
+        if (squared < x) {
+            // increase unless within 1 int
+            if (lastSqrt - sqrt < 2) {
+                break;
+            }
+            sqrt += getPivot(lastSqrt - sqrt);
+        } else {
+            // decrease
+            lastSqrt = sqrt;
+            sqrt = getPivot(sqrt);
+        }
+        squared = sqrt * sqrt;
+    }
+    return sqrt;
 };
