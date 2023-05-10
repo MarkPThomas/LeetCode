49e140d5ad0080f4afe1e84f586a4a203a35fb0e
Mark P Thomas <markums@gmail.com>
2023-05-10 11:30:11 -0600

    Removed unused testing template

--- 0070_M_ClimbingStairs.js
+++ 0070_M_ClimbingStairs.js
@@ -3,15 +3,3 @@
 // Time to complete: xx min
 // Patterns:
 // Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-  }
-);
