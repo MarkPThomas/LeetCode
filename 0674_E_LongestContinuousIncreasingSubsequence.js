f7e55c9e6f2c48400d9f520af9852ae3ff4b2464
Mark P Thomas <markums@gmail.com>
2023-05-10 11:29:37 -0600

    Completed problem

--- 0674_E_LongestContinuousIncreasingSubsequence.js
+++ 0674_E_LongestContinuousIncreasingSubsequence.js
@@ -1,5 +1,23 @@
-// O() time complexity
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: 3 min
+// Patterns: Sliding window
 // Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  let lengthIncrease = 1;
+  let maxLengthIncrease = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      lengthIncrease++;
+    } else {
+      maxLengthIncrease = Math.max(maxLengthIncrease, lengthIncrease);
+      lengthIncrease = 1;
+    }
+  }
+
+  return Math.max(maxLengthIncrease, lengthIncrease);
+};
