7c5396214a34ea4a3fa19bbd3dbaf42fba1714c1
Mark P Thomas <markums@gmail.com>
2023-05-25 16:45:15 -0700

    Optimized solution to have O(1) space complexity rather than O(n)

--- 0238_M_ProductOfArrayExceptSelf.js
+++ 0238_M_ProductOfArrayExceptSelf.js
@@ -1,6 +1,6 @@
 // O(n) time complexity
-// O(n) space complexity
-// Time to complete: 27 min
+// O(1) space complexity
+// Time to complete: 27 min + 3 min (for optimization)
 // Patterns: Array/string.
 // Notes w.r.t. solution: Took a 'hail mary' hint about 15 min in, but worked out the solution from there pretty fast.
 
@@ -10,15 +10,14 @@
  */
 var productExceptSelf = function (nums) {
   const productLeft = [1];
-  const productRight = [1];
   for (let i = 1; i < nums.length; i++) {
     productLeft.push(productLeft[i - 1] * nums[i - 1]);
-    productRight.push(productRight[i - 1] * nums[nums.length - i]);
   }
 
-  const answer = [];
-  for (let i = 0; i < productLeft.length; i++) {
-    answer.push(productLeft[i] * productRight[productRight.length - 1 - i]);
+  let rightProduct = 1;
+  for (let i = productLeft.length - 2; 0 <= i; i--) {
+    rightProduct *= nums[i + 1];
+    productLeft[i] *= rightProduct;
   }
-  return answer;
+  return productLeft;
 };
