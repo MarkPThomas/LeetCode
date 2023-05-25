b9b5b3d123ea3ba791b070b40a4c054e4fce8a58
Mark P Thomas <markums@gmail.com>
2023-05-25 13:27:24 -0700

    Completed problems

---
+++ 0283_E_MoveZeros.js
@@ -0,0 +1,22 @@
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 4 min
+// Patterns: 2 Pointers
+// Notes w.r.t. solution:
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let firstZeros = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0 && firstZeros === -1) {
+      firstZeros = i;
+    } else if (nums[i] !== 0 && firstZeros !== -1) {
+      nums[firstZeros] = nums[i];
+      nums[i] = 0;
+      firstZeros++;
+    }
+  }
+};
--- 0665_E_NonDecreasingArray.js
+++ 0665_E_NonDecreasingArray.js
@@ -1,5 +1,36 @@
-// O() time complexity
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: 38 min
+// Patterns: Greedy
+// Notes w.r.t. solution: Was a bit too fast in implementation & corrections, rather than thinking it through.
+//      Also modifying array (since allowed) made things simpler.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function (nums) {
+  if (nums.length < 2) {
+    return true;
+  }
+
+  let decreasingCount = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[i - 1]) {
+      decreasingCount++;
+      if (decreasingCount === 1) {
+        if (i < nums.length - 1 && nums[i - 1] > nums[i + 1]) {
+          nums[i - 1] = nums[i];
+          if (1 < i && nums[i - 1] < nums[i - 2]) {
+            return false;
+          }
+        } else {
+          nums[i] = nums[i - 1];
+        }
+      } else if (decreasingCount > 1) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
