2da2cddd674f0dfbecb818af72f1d11eff3aadd0
Mark P Thomas <markums@gmail.com>
2023-05-26 09:03:29 -0700

    Completed problem

---
+++ 0392_E_IsSubsequence.js
@@ -0,0 +1,20 @@
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: xx min
+// Patterns: 2 Pointer
+// Notes w.r.t. solution:
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let ptrS = 0;
+  for (let ptrT = 0; ptrT < t.length; ptrT++) {
+    if (s[ptrS] === t[ptrT]) {
+      ptrS++;
+    }
+  }
+  return ptrS === s.length;
+};
