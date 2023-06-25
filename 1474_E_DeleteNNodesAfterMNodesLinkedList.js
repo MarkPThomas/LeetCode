ec1870cf872ecb3cf75b986ca9bf353386804ccd
Mark P Thomas <markums@gmail.com>
2023-06-25 11:43:54 -0500

    Completed problem

---
+++ 1474_E_DeleteNNodesAfterMNodesLinkedList.js
@@ -0,0 +1,47 @@
+// O() time complexity
+// O() space complexity
+// Time to complete: 19 min (wrong method), 9 min (right method)
+// Patterns: Linked Lists
+// Notes w.r.t. solution: Was unnclear about handling remainder nodes. This required working a different solution.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} m
+ * @param {number} n
+ * @return {ListNode}
+ */
+var deleteNodes = function (head, m, n) {
+  let node = head;
+  let prevNode = null;
+  let counterM = 1;
+  let counterN = 1;
+  while (node) {
+    if (counterM % m === 0) {
+      prevNode = node;
+      while (node && counterN % (n + 1)) {
+        node = node.next;
+        prevNode.next = node;
+        counterN++;
+      }
+      if (node) {
+        node = node.next;
+        prevNode.next = node;
+      }
+      counterM = 1;
+      counterN = 1;
+      prevNode = prevNode.next;
+    } else {
+      prevNode = node;
+      node = node.next;
+      counterM++;
+    }
+  }
+  return head;
+};
