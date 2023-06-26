c7d35fd8a9e4de8f26e17ed87622fa0a6e63384e
Mark P Thomas <markums@gmail.com>
2023-06-26 04:59:19 -0500

    Completed problem

---
+++ 0760_E_FindAnagramMappings.js
@@ -0,0 +1,60 @@
+// O() time complexity
+// O() space complexity
+// Time to complete: 3:00 min w/ array queue, 11:00 w/ linked list queue
+// Patterns:
+// Notes w.r.t. solution:
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var anagramMappings = function (nums1, nums2) {
+  const indices = {};
+  for (let i = 0; i < nums2.length; i++) {
+    if (!indices[nums2[i]]) {
+      indices[nums2[i]] = new QueueLL();
+    }
+    indices[nums2[i]].enqueue(i);
+  }
+
+  const mapping = [];
+  for (let i = 0; i < nums1.length; i++) {
+    let numIndices = indices[nums1[i]];
+    const val = numIndices.dequeue();
+    mapping.push(val);
+  }
+  return mapping;
+};
+
+class Node {
+  val;
+  next;
+  constructor(val) {
+    this.val = val;
+  }
+}
+
+class QueueLL {
+  head = null;
+  tail = null;
+
+  enqueue(val) {
+    const node = new Node(val);
+    if (this.tail) {
+      this.tail.next = node;
+      this.tail = this.tail.next;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+  }
+
+  dequeue() {
+    let removedNode = this.head;
+    if (removedNode) {
+      this.head = this.head.next;
+      return removedNode.val;
+    }
+  }
+}
