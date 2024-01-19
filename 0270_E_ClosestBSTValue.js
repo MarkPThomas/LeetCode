44186a0d5f0949f74f5d791a374e6a56b9c50164
Mark P Thomas <markums@gmail.com>
2024-01-19 10:53:31 -0700

    Solved

---
+++ 0270_E_ClosestBSTValue.js
@@ -0,0 +1,48 @@
+// O(H) time complexity where H = height of binary tree
+// O(1) space complexity
+// Time to complete: 30:00 min, 2:00 extra to simplify code
+// Patterns: Binary Search Tree
+// Notes w.r.t. solution: Found best solution!
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function (root, target) {
+  if (root.val === target) {
+    return target;
+  }
+
+  let closest = root.val;
+  let closestAlt = null;
+
+  while (root !== null) {
+    const val = root.val;
+
+    if (val === target) {
+      return target;
+    } else {
+      const deltaClosest = Math.abs(target - closest);
+      const deltaCurrent = Math.abs(target - val);
+      if (deltaCurrent < deltaClosest) {
+        closest = val;
+        closestAlt = Infinity;
+      } else if (deltaCurrent === deltaClosest) {
+        closestAlt = val;
+      }
+
+      root = target < root.val ? root.left : root.right;
+    }
+  }
+
+  return closestAlt !== null ? Math.min(closest, closestAlt) : closest;
+};
