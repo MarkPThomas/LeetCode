c365d9a11807b631511a057b199166b8d9ab8545
Mark P Thomas <markums@gmail.com>
2024-01-22 10:07:35 -0700

    Improved solution

--- 0604_E_DesignCompressedStringIterator.js
+++ 0604_E_DesignCompressedStringIterator.js
@@ -1,4 +1,72 @@
-// Final, optimized solution
+// Final, most optimized solution
+// O(1) time complexity
+// O(1) space complexity
+// Time to complete: 16:00, although I wasn't tripped up by edge cases or needing to remember some functions/JS stuff.
+// Patterns: Process on demand
+// Notes w.r.t. solution: Optimized by storing a reference to the string and processing on demand while using a pointer.
+/**
+ * @param {string} compressedString
+ */
+var StringIterator = function (compressedString) {
+  this.compressedString = compressedString;
+  this.ptr = 0;
+  this.currentCount = 0;
+  this.nextCount = 0;
+
+  this.calcNextCount();
+};
+
+StringIterator.prototype.calcNextCount = function () {
+  let i = this.ptr + 1;
+  let ints = '';
+  while (i < this.compressedString.length
+    && !isNaN(parseInt(this.compressedString[i]))) {
+    ints += this.compressedString[i];
+    i++;
+  }
+
+  this.nextCount = parseInt(ints);
+}
+
+StringIterator.prototype.movePtr = function () {
+  this.ptr += (this.nextCount.toString().length + 1);
+}
+
+/**
+* @return {character}
+*/
+StringIterator.prototype.next = function () {
+  if (this.hasNext()) {
+    const char = this.compressedString[this.ptr];
+
+    this.currentCount++;
+    if (this.currentCount === this.nextCount) {
+      this.currentCount = 0;
+      this.movePtr();
+      this.calcNextCount();
+    }
+
+    return char;
+  } else {
+    return ' ';
+  }
+};
+
+/**
+* @return {boolean}
+*/
+StringIterator.prototype.hasNext = function () {
+  return this.ptr + this.nextCount.toString().length < this.compressedString.length;
+};
+
+/**
+* Your StringIterator object will be instantiated and called as such:
+* var obj = new StringIterator(compressedString)
+* var param_1 = obj.next()
+* var param_2 = obj.hasNext()
+*/
+
+// More optimized solution
 // O(1) time complexity
 // O(n) space complexity
 // Time to complete: 27:37 min (originally 12:20 but missed a few edge cases. A bit too rushed!) + 11:09 to optimize array into linked list
