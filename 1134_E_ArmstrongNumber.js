db5e9d3b8e3b2579162cbccd14d0a17f272d5b47
Mark P Thomas <markums@gmail.com>
2024-02-04 13:02:01 -0700

    Completed problem

---
+++ 1134_E_ArmstrongNumber.js
@@ -0,0 +1,52 @@
+// O(m) time complexity
+// O(m) space complexity
+//  where m = # digits in n.
+// Time to complete: 5:30 min for string, 9:47 for pure numeric
+// Patterns: Picking off integers
+// Notes w.r.t. solution:
+
+// ==== Purely Numeric ===
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isArmstrong = function (n) {
+  let tempN = n;
+
+  let numDigits = 0;
+  while (tempN) {
+    numDigits++;
+    tempN = Math.floor(tempN / 10);
+  }
+
+  tempN = n;
+  let tempSum = 0;
+  while (tempN) {
+    tempSum += (Math.floor(tempN % 10)) ** numDigits;
+    tempN = Math.floor(tempN / 10);
+  }
+
+  return tempSum === n;
+};
+
+
+// ==== With strings ====
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isArmstrong = function (n) {
+  let nString = n.toString();
+  let numDigits = [];
+
+  for (let i = 0; i < nString.length; i++) {
+    numDigits.push(parseInt(nString[i]))
+  }
+
+  let armstrongSum = 0;
+  numDigits.forEach((digit) => {
+    armstrongSum += digit ** numDigits.length;
+  });
+
+  return armstrongSum === n;
+};
