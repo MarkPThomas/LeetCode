57550b90ea0ffb8f28f6435cccbe3d94d2ab34a3
Mark P Thomas <markums@gmail.com>
2024-03-05 09:36:54 -0700

    Re-solved problem

--- 0252_E_MeetingRooms.js
+++ 0252_E_MeetingRooms.js
@@ -1,3 +1,27 @@
+// 2024/03
+// O(n*log(n)) time complexity
+// O(1) space complexity
+// Time to complete: 3:38 min
+// Patterns: Merge intervals
+// Notes w.r.t. solution:
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < intervals.length - 1; i++) {
+    if (intervals[i][1] > intervals[i + 1][0]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// 2023/05
 // O(n * log(n)) time complexity
 // O(1) space complexity
 // Time to complete: 5 min
@@ -8,7 +32,7 @@
  * @param {number[][]} intervals
  * @return {boolean}
  */
-var canAttendMeetings = function (intervals) {
+var canAttendMeetings202305 = function (intervals) {
   if (intervals.length < 2) {
     return true;
   }
