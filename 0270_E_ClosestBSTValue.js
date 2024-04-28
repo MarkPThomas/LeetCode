d343699ed8ede0771fc933875693d2dc379a5354
Mark P Thomas <markums@gmail.com>
2024-04-28 15:17:48 -0600

    Reworked for 2024

--- 0270_E_ClosestBSTValue.js
+++ 0270_E_ClosestBSTValue.js
@@ -1,3 +1,43 @@
+// 2024/04/28
+// O(H) time complexity where H = height of binary tree
+// O(1) space complexity
+// Time to complete: 6:20 min
+// Patterns: Binary Search Tree, DFS
+// Notes w.r.t. solution: Found best solution!
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function (root, target) {
+  let node = root;
+  let max = Infinity;
+  let min = -Infinity;
+
+  while (node) {
+    if (node.val === target) {
+      return target;
+    } else if (target < node.val) {
+      max = Math.min(max, node.val);
+      node = node.left;
+    } else {
+      min = Math.max(min, node.val);
+      node = node.right;
+    }
+  }
+
+  return Math.abs(max - target) < Math.abs(target - min) ? max : min;
+};
+
+// 2024/01/19
 // O(H) time complexity where H = height of binary tree
 // O(1) space complexity
 // Time to complete: 30:00 min, 2:00 extra to simplify code
--- 1207_E_UniqueNumberOfOccurrences.js
+++ 1207_E_UniqueNumberOfOccurrences.js
@@ -1,3 +1,40 @@
+// 2024/04/28
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 3:47 min
+// Patterns: Hashmap
+// Notes w.r.t. solution: Would have been faster but messed of for of loop & switched to more verbose form.
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+  const freq = {};
+  arr.forEach((num) => {
+    if (!freq[num]) {
+      freq[num] = 0;
+    }
+
+    freq[num]++;
+  });
+
+  const numOccur = {};
+  const keys = Object.keys(freq);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const value = freq[key];
+
+    if (!numOccur[value]) {
+      numOccur[value] = key;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// 2023/05
 // O(n) time complexity
 // O(n) space complexity
 // Time to complete: 7:00 min
