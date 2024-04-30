d5e158596f2701da6a9cfd9f83453c19f0465cce
Mark P Thomas <markums@gmail.com>
2024-04-29 20:01:33 -0600

    Reworked for 2024

--- 2215_E_DifferenceOfTwoArrays copy.js
+++ 2215_E_DifferenceOfTwoArrays copy.js
@@ -1,3 +1,42 @@
+// 2024/04/29
+// O(n + m) time complexity
+// O(n + m) space complexity
+// where n = size of nums1, m = size of nums2
+// Time to complete: 5:20 min
+// Patterns: Hashmap
+// Notes w.r.t. solution: Lost 1 min by accidentally using the values & not keys of the hashmaps
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function (nums1, nums2) {
+  const notPresentIn2 = {};
+  nums1.forEach((num1) => {
+    notPresentIn2[num1] = true;
+  });
+
+  nums2.forEach((num2) => {
+    if (notPresentIn2[num2]) {
+      delete notPresentIn2[num2];
+    }
+  });
+
+  const notPresentIn1 = {};
+  nums2.forEach((num2) => {
+    notPresentIn1[num2] = true;
+  })
+
+  nums1.forEach((num1) => {
+    if (notPresentIn1[num1]) {
+      delete notPresentIn1[num1];
+    }
+  });
+
+  return [Object.keys(notPresentIn2), Object.keys(notPresentIn1)];
+}
+
+// 2023/05
 // O(n + m) time complexity
 // O(max(n,m)) space complexity
 // where n = size of nums1, m = size of nums2
