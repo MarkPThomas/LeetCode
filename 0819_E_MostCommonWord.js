606681f7ba2d5f490b5f523785d80ca9b0d2c0e8
Mark P Thomas <markums@gmail.com>
2024-05-03 09:24:48 -0600

    Solved for 2024

--- 0819_E_MostCommonWord.js
+++ 0819_E_MostCommonWord.js
@@ -1,5 +1,47 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/04/30
+// O(n + m) time complexity
+// O(n + m) space complexity
+// where n = # paragraph words, m = # banned words
+// Time to complete: 11:14 min
+// Patterns: Hashmap, strings
+// Notes w.r.t. solution: String BS to work out, unclear setup/examples cost more time not recorded.
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function (paragraph, banned) {
+  const bannedWords = {};
+  banned.forEach((word) => {
+    bannedWords[word] = true;
+  });
+
+  const regex1 = /[\']/gi;
+  paragraph = paragraph.replaceAll(regex1, '').toLowerCase();
+
+  const regex2 = /[!?,;.]/gi;
+  paragraph = paragraph.replaceAll(regex2, ' ').toLowerCase();
+
+  const words = paragraph.split(' ');
+
+  let maxCount = 0;
+  const wordFreq = {};
+
+  words.forEach((word) => {
+    if (word && !bannedWords[word]) {
+      if (!wordFreq[word]) {
+        wordFreq[word] = 0;
+      }
+      wordFreq[word]++;
+      maxCount = Math.max(maxCount, wordFreq[word]);
+    }
+  });
+
+  for ([key, value] of Object.entries(wordFreq)) {
+    if (value === maxCount) {
+      return key;
+    }
+  }
+
+  return '';
+};
