a3c0d61fd6cfc1983f3b123a44ea1aec1815b361
Mark P Thomas <markums@gmail.com>
2024-05-04 19:09:05 -0600

    Solved for 2024

--- 0118_E_PascalsTriangle.js
+++ 0118_E_PascalsTriangle.js
@@ -1,5 +1,38 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/05/04
+// O(n^2) time complexity
+// O(1) space complexity if ignoring output
+// Time to complete: 19:30 min
+// Patterns: Dynamic - Tabulation
+// Notes w.r.t. solution: Would have been more like 6-10 min but was getting tripped up on indices.
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let result = [];
+
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+
+    for (let j = 0; j < i + 1; j++) {
+      let sum = 0;
+
+      if (i === 0) {
+        sum += 1;
+      } else if (j < i) {
+        sum += result[i - 1][j];
+      }
+
+      if (i > 0 && j > 0) {
+        sum += result[i - 1][j - 1];
+      }
+
+      row.push(sum);
+    }
+
+    result.push(row)
+  }
+
+  return result;
+};
--- 0268_E_MissingNumber.js
+++ 0268_E_MissingNumber.js
@@ -1,5 +1,31 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/05/04
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 13:50 min
+// Patterns: Cyclic Sort
+// Notes w.r.t. solution: Review cyclic sort. Was basically there early on.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  // cyclic sort
+  let i = 0;
+  while (i < nums.length) {
+    targetIdx = nums[i];
+    if (nums[i] !== nums[targetIdx]) {
+      [nums[i], nums[targetIdx]] = [nums[targetIdx], nums[i]];
+    } else {
+      i++;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i) {
+      return i;
+    }
+  }
+
+  return nums.length;
+};
