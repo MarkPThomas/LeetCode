25cc0e8ec6937a592720da8ca3286a421e03f428
Mark P Thomas <markums@gmail.com>
2024-05-15 12:01:34 -0600

    Reworked with iterative solution

--- 0094_M_BinaryTreeInorderTraversal.js
+++ 0094_M_BinaryTreeInorderTraversal.js
@@ -1,3 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const values = [];
+  const stack = [];
+  let curr = root;
+
+  // If not at null or if any processing left
+  while (curr || stack.length) {
+    // Fill stack up left-most branch
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    // Process stack at current level
+    curr = stack.pop();
+    values.push(curr.val);
+
+    // Halt processing & check right branch
+    curr = curr.right;
+  }
+
+  return values;
+};
+
+// 2023/04
 // O(n) time complexity
 // O(n) space complexity
 // Time to complete: 1:00 min
