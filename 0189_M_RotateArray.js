e9d34e5f14cb6cf728e988397b952f43b8297571
Mark P Thomas <markums@gmail.com>
2024-05-17 08:03:41 -0600

    Reworked for 2024

--- 0189_M_RotateArray.js
+++ 0189_M_RotateArray.js
@@ -1,3 +1,33 @@
+// 2024/05/16
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 15:52 min
+// Patterns: Math
+// Notes w.r.t. solution: 10:10 min to solve with non-working cyclic sorting.
+//    Another 5:42 min to modifify to just use auxiliary array.
+//    Would have been under 10 min if I had just done this first.
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  // update k for when k > length;
+  k = k % nums.length;
+
+  const result = [...nums];
+  for (let i = 0; i < result.length; i++) {
+    let targetIdx = i + k;
+    if (targetIdx < result.length) {
+      nums[targetIdx] = result[i];
+    } else {
+      nums[targetIdx - result.length] = result[i];
+    }
+  }
+};
+
+
+// 2023/04
 // O(n) time complexity
 // O(1) space complexity
 // Time to complete: too long :-( min
@@ -29,18 +59,3 @@ var rotate = function (nums, k) {
     } while (startIndex !== currentIndex)
   }
 };
-
-
-const testCases = [
-  {
-    input: '',
-    expected: ''
-  },
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-}
-);
