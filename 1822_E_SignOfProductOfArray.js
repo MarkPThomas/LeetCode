a0575761f14b8018d30e84ec10716e98a44fef8c
Mark P Thomas <markums@gmail.com>
2024-08-31 19:10:40 -0600

    Completed for 2024

--- 1822_E_SignOfProductOfArray.js
+++ 1822_E_SignOfProductOfArray.js
@@ -1,5 +1,25 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/08/31
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 2:27 min
+// Patterns: Math
 // Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function (nums) {
+  let sign = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      return 0;
+    }
+
+    if (nums[i] < 0) {
+      sign *= -1;
+    }
+  }
+
+  return sign;
+};
