6234e74db7e75a1813e0ced8f947a435b7501af5
Mark P Thomas <markums@gmail.com>
2024-09-02 14:36:30 -0600

    Solved for 2024

--- 0077_M_Combinations.js
+++ 0077_M_Combinations.js
@@ -1,6 +1,60 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/09/02
+// O(n^k) time complexity
+// O(n^k) space complexity
+// Time to complete: 36:50 min
+// Patterns: Backtracking
 // Notes w.r.t. solution:
+//    Mostly solved in 18:08
+//    Debugging to 25:00 showed I had repeated solutions
+//    Took until 36:50 to work out how to check for non-unique solutions
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+  const solutions = [];
 
+  function isDone(solution, k) {
+    return solution.length === k;
+  }
+
+  function isValid(candidate, usedCandidates, solution) {
+    return (!usedCandidates[candidate]
+      && (!solution.length || solution[solution.length - 1] < candidate));
+  }
+
+  function place(candidate, usedCandidates) {
+    usedCandidates[candidate] = true;
+  }
+
+  function remove(candidate, usedCandidates) {
+    delete usedCandidates[candidate];
+  }
+
+  function backtrack(values, k, usedCandidates) {
+    const solution = Object.keys(usedCandidates);
+    if (isDone(solution, k)) {
+      solutions.push(solution);
+      return;
+    }
+
+    values.forEach((candidate) => {
+      if (isValid(candidate, usedCandidates, solution)) {
+        place(candidate, usedCandidates);
+        backtrack(values, k, usedCandidates);
+        remove(candidate, usedCandidates);
+      }
+    });
+  }
+
+  const values = [];
+  for (i = 1; i <= n; i++) {
+    values.push(i);
+  }
+
+  const usedCandidates = {};
+  backtrack(values, k, usedCandidates);
+
+  return solutions;
+};
--- 1523_E_CountOddNumbersInIntervalRange.js
+++ 1523_E_CountOddNumbersInIntervalRange.js
@@ -1,5 +1,21 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/09/02
+// O(1) time complexity
+// O(1) space complexity
+// Time to complete: 4:10 min
+// Patterns: Math
 // Notes w.r.t. solution:
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+  // for each #:
+  // if even, divide by 2
+  // if odd, add 1 to high (or subtract 1 for low) & then divide by 2
+  // subtract low from high
+  const lowCountInclusive = low % 2 ? (low - 1) / 2 : low / 2;
+  const highCount = high % 2 ? (high + 1) / 2 : high / 2;
+
+  return highCount - lowCountInclusive;
+};
