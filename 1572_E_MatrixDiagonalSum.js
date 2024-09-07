769a12f3d1db37ee41bfe748931df925f91f3068
Mark P Thomas <markums@gmail.com>
2024-09-07 12:15:06 -0600

    Completed for 2024

--- 1572_E_MatrixDiagonalSum.js
+++ 1572_E_MatrixDiagonalSum.js
@@ -1,5 +1,23 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
+// 2024/09/07
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 5:37 min
 // Patterns:
 // Notes w.r.t. solution:
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+  let sum = 0;
+  for (let i = 0; i < mat.length; i++) {
+    sum += mat[i][i];
+
+    let colIdx = mat.length - 1 - i;
+    if (i !== colIdx) {
+      sum += mat[i][colIdx];
+    }
+  }
+
+  return sum;
+};
