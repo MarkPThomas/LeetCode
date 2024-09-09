e33683bcb342b3151ba8cd3fe94ff723b5261872
Mark P Thomas <markums@gmail.com>
2024-09-09 16:40:32 -0600

    Solved for 2024

--- 0022_M_GenerateParentheses.js
+++ 0022_M_GenerateParentheses.js
@@ -1,5 +1,35 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/09/09
+// O(2^(2n)) -> O(4^n) time complexity
+// O(n) space complexity
+// Time to complete: 11:43 min
+// Patterns: Backtracking
 // Notes w.r.t. solution:
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const results = [];
+
+  function backtrack(open, closed, result) {
+    if (closed === open && open === n) {
+      results.push(result.join(''));
+      return;
+    }
+
+    if (open < n) {
+      result.push('(');
+      backtrack(open + 1, closed, result);
+      result.pop();
+    }
+
+    if (closed < open) {
+      result.push(')');
+      backtrack(open, closed + 1, result);
+      result.pop();
+    }
+  }
+
+  backtrack(0, 0, []);
+  return results;
+};
--- 0157_E_ReadNCharsGivenRead4.js
+++ 0157_E_ReadNCharsGivenRead4.js
@@ -1,5 +1,54 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
+// 2024/09/09
+// O(n) time complexity
+// O(1) space complexity, since buf is ignored but tempBuf only has at most 4 chars
+// Time to complete: 16:09 min
 // Patterns:
 // Notes w.r.t. solution:
+/**
+ * Definition for read4()
+ *
+ * @param {character[]} buf4 Destination buffer
+ * @return {number} The number of actual characters read
+ * read4 = function(buf4) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} read4()
+ * @return {function}
+ */
+var solution = function (read4) {
+  /**
+   * @param {character[]} buf Destination buffer
+   * @param {number} n Number of characters to read
+   * @return {number} The number of actual characters read
+   */
+  return function (buf, n) {
+    let lastRead = 0;
+    let totalRead = 0;
+    const max = Math.ceil(n / 4);
+
+    for (let i = 0; i < max; i++) {
+      const tempBuf = [];
+
+      lastRead = read4(tempBuf);
+      if (!lastRead) {
+        break;
+      }
+
+      totalRead += lastRead;
+
+      if (totalRead > n) {
+        const over = totalRead - n;
+        const redBuf = tempBuf.slice(0, tempBuf.length - over);
+        buf.push(...redBuf);
+        totalRead = n;
+      } else {
+        buf.push(...tempBuf);
+      }
+    }
+
+    return totalRead;
+  };
+};
