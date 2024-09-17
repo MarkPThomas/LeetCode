5d0c89e9b2c6546abfecfc9b56ee84c55ae6a92d
Mark P Thomas <markums@gmail.com>
2024-09-17 10:41:30 -0600

    Added refactored/improved solution

--- 0150_M_EvaluateReversePolishNotation.js
+++ 0150_M_EvaluateReversePolishNotation.js
@@ -2,6 +2,44 @@
 // O(n) space complexity
 // Time to complete: 20:43 min
 // Patterns: Stack
+// Notes w.r.t. solution: Refactored & optimized from answer.
+//    Adding here for comparative reference.
+//    Creating lambda functions I could/should have thought of.
+//    Math's trunc feature was new & good to remember!
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => Math.trunc(a / b),
+  }
+
+  const results = [];
+  for (const token of tokens) {
+    if (token in operations) {
+      const b = results.pop();
+      const a = results.pop();
+      const operation = operations[token];
+
+      const result = operation(a, b);
+      results.push(result);
+
+    } else {
+      results.push(parseInt(token));
+    }
+  }
+
+  return results.pop();
+};
+
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 20:43 min
+// Patterns: Stack
 // Notes w.r.t. solution: Was 11:46, but had minor bugs & troubleshooting of division
 /**
  * @param {string[]} tokens
