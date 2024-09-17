c092f374e8ac5f191ce6bf5844982e283a28f4fd
Mark P Thomas <markums@gmail.com>
2024-09-17 10:29:22 -0600

    Completed for 2024

--- 0150_M_EvaluateReversePolishNotation.js
+++ 0150_M_EvaluateReversePolishNotation.js
@@ -1,17 +1,47 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 20:43 min
+// Patterns: Stack
+// Notes w.r.t. solution: Was 11:46, but had minor bugs & troubleshooting of division
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const operators = {
+    '+': true,
+    '-': true,
+    '*': true,
+    '/': true,
   }
-);
+
+  const operations = [];
+  for (const token of tokens) {
+    if (operators[token]) {
+      const b = operations.pop();
+      const a = operations.pop();
+
+      let result = 0;
+      if (token === '+') {
+        result = a + b;
+      } else if (token === '-') {
+        result = a - b;
+      } else if (token === '*') {
+        result = a * b;
+      } else if (token === '/') {
+        result = a / b;
+        if (result < 0) {
+          result = Math.ceil(result);
+        } else {
+          result = Math.floor(result);
+        }
+      }
+      operations.push(result);
+
+    } else {
+      operations.push(parseInt(token));
+    }
+  }
+
+  return operations.pop();
+};
--- 0680_E_ValidPalindromeII.js
+++ 0680_E_ValidPalindromeII.js
@@ -1,5 +1,37 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 17:00 min
+// Patterns: 2 Pointers
+// Notes w.r.t. solution: Initially lost a lot of time with a minor bug due to if/else statements
+//    causing only one viable path to be explored. Calling a function w/ 2 states & 'or' is necessary.
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  function checkPalindrome(left, right) {
+    while (left < right) {
+      if (s[left] !== s[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return (checkPalindrome(left, right - 1)
+        || checkPalindrome(left + 1, right));
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
