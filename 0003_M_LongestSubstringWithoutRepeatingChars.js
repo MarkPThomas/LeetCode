c234a142658057daa56a53d0f108f46e2b3ed68b
Mark P Thomas <markums@gmail.com>
2024-09-19 19:10:38 -0600

    Solved for 2024

--- 0003_M_LongestSubstringWithoutRepeatingChars.js
+++ 0003_M_LongestSubstringWithoutRepeatingChars.js
@@ -1,5 +1,38 @@
-// O() time complexity
+// 2024/09/19
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: 26:00 min
+// Patterns: Hashmap, 2 pointer
 // Notes w.r.t. solution:
+//   Lost 5 min due to hashmap val = 0 oopsie. Deducted
+//   Mostly solved in 16-22 min. Final bit had to do with consideration of idxStart update & length math
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let longestSubstr = 0;
+  let idxStart = 0;
+
+  function getLongestSubtrLength(idxCurr) {
+    const currSubstrLength = idxCurr - idxStart;
+    longestSubstr = Math.max(longestSubstr, currSubstrLength);
+  }
+
+  let currSubstr = {}
+  for (let i = 0; i < s.length; i++) {
+    if (currSubstr[s[i]] !== undefined) {
+      getLongestSubtrLength(i);
+
+      if (idxStart <= currSubstr[s[i]]) {
+        idxStart = currSubstr[s[i]] + 1;
+      }
+    }
+
+    currSubstr[s[i]] = i;
+  }
+
+  getLongestSubtrLength(s.length);
+
+  return longestSubstr;
+};
--- 0844_E_BackspaceStringCompare.js
+++ 0844_E_BackspaceStringCompare.js
@@ -1,5 +1,86 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/09/19
+// O(m + n) time complexity
+// O(1) space complexity
+// where m = length of s, n = length of t
+// Time to complete: 17:04 min
+// Patterns: 2 Pointer
 // Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+  function movePtr(s, sIdx) {
+    if (sIdx < 0) {
+      return sIdx;
+    }
+
+    let count = 0;
+    while (s[sIdx] === '#' || count) {
+      if (s[sIdx] === '#') {
+        count++;
+      } else if (count) {
+        count--;
+      }
+      sIdx--;
+    }
+
+    return sIdx;
+  }
+
+  let sIdx = s.length - 1;
+  let tIdx = t.length - 1;
+  while (0 <= sIdx && 0 <= tIdx) {
+    sIdx = movePtr(s, sIdx);
+    tIdx = movePtr(t, tIdx);
+
+    if (s[sIdx] !== t[tIdx]) {
+      return false;
+    }
+    sIdx--;
+    tIdx--;
+  }
+
+  sIdx = movePtr(s, sIdx);
+  tIdx = movePtr(t, tIdx);
+
+  return sIdx < 0 && tIdx < 0;
+};
+
+
+// 2024/09/19
+// O(m + n) time complexity
+// O(m + n) space complexity
+// where m = length of s, n = length of t
+// Time to complete: 5:04 min
+// Patterns: Stack
+// Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+  // reduce s & t
+  function reduce(s) {
+    const sStack = [];
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === '#') {
+        sStack.pop();
+      } else {
+        sStack.push(s[i]);
+      }
+    }
+    return sStack;
+  }
+
+  const sStack = reduce(s);
+  const tStack = reduce(t);
+
+  // compare s & t
+  if (sStack.length !== tStack.length) {
+    return false;
+  }
+  return sStack.join('') === tStack.join('');
+};
