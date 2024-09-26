c271887577b353c171668ed7019c4ce3c8f209b7
Mark P Thomas <markums@gmail.com>
2024-09-26 11:26:29 -0600

    Completed for 2024

--- 0102_M_BinaryTreeFromLevelOrderTraversal.js
+++ 0102_M_BinaryTreeFromLevelOrderTraversal.js
@@ -1,3 +1,58 @@
+// 2024/09/26
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 5:25 min
+// Patterns: BFS, Iteration
+// Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) {
+    return [];
+
+  }
+  const output = [];
+  let row = [];
+
+  const nodes = [root, null];
+  while (nodes.length) {
+    const node = nodes.shift();
+
+    if (node) {
+      row.push(node.val);
+
+      if (node.left) {
+        nodes.push(node.left);
+      }
+
+      if (node.right) {
+        nodes.push(node.right);
+      }
+    } else {
+      output.push([...row]);
+      row = [];
+
+      if (nodes.length) {
+        nodes.push(null);
+
+      }
+    }
+
+  }
+
+  return output;
+};
+
 // 2024/09/11
 // O(n) time complexity
 // O(n) space complexity
--- 0133_M_CloneGraph.js
+++ 0133_M_CloneGraph.js
@@ -1,17 +1,64 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// O(n + m) time complexity
+// O(n) space complexity
+//  where n = # nodes
+//    m = # edges
+// Time to complete: 11:22 min
+// Patterns: Graph, DFS w/ Iteration
 // Notes w.r.t. solution:
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {
+  if (!node) {
+    return node;
   }
-);
+
+  const cloned = {};
+  const nodes = [node];
+  while (nodes.length) { // T: O(n), S: O(n)
+    const node = nodes.pop();
+    if (cloned[node.val]) {
+      continue;
+    }
+
+    const clone = new Node(node.val);
+    cloned[node.val] = clone;
+
+    for (const neighbor of node.neighbors) {
+      if (!cloned[neighbor.val]) {
+        nodes.push(neighbor);
+      }
+    }
+  }
+
+  const linked = {};
+  nodes.push(node);
+  while (nodes.length) { // T: O(n + m), S: O(n)
+    const node = nodes.pop();
+    if (linked[node.val]) {
+      continue;
+    }
+
+    const clone = cloned[node.val];
+    for (const neighbor of node.neighbors) {
+      clone.neighbors.push(cloned[neighbor.val]);
+
+      if (!linked[neighbor.val]) {
+        nodes.push(neighbor);
+      }
+    }
+
+    linked[node.val] = clone;
+  }
+
+  return cloned[1];
+};
