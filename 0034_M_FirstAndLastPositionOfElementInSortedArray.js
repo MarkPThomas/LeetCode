389489f45959380a7d618c75612f8a6c4b05080f
Mark P Thomas <markums@gmail.com>
2024-09-30 13:23:56 -0600

    Completed for 2024

--- 0034_M_FirstAndLastPositionOfElementInSortedArray.js
+++ 0034_M_FirstAndLastPositionOfElementInSortedArray.js
@@ -1,17 +1,45 @@
-// O() time complexity
+// 2024/09/30
+// O(n * log(n)) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: 12:54 min
+// Patterns: Binary Search
+// Notes w.r.t. solution: Would have been solved in 7:30, but had minor errors in code. Doh!
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  // Find start
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
-);
+
+  if (nums[left] !== target) {
+    return [-1, -1];
+  }
+  const start = left;
+
+  // Find end
+  right = nums.length - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return [start, right];
+  // return [start, Math.max(start, left - 1)];
+};
--- 0151_M_ReverseWordsInString.js
+++ 0151_M_ReverseWordsInString.js
@@ -1,3 +1,59 @@
+// 2024/09/30
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 14:47 min
+// Patterns: 2 Pointers
+// Notes w.r.t. solution: Beware substr() vs. substring().
+//    substr() is deprecated, but also signature is different - NOT startI, endI (noninclusive)
+//    Also, empty chars are not nullable, only no chars
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  const words = [];
+  let startChar = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      if (s[startChar] !== ' ') {
+        const substring = s.substring(startChar, i);
+        if (substring !== ' ') {
+          words.push(substring);
+        }
+      }
+      startChar = i + 1;
+      continue;
+    }
+  }
+
+  if (startChar !== s.length) {
+    words.push(s.substring(startChar, s.length));
+  }
+
+  let left = 0;
+  let right = words.length - 1;
+  while (left < right) {
+    if (!words[left]) {
+      left++;
+      continue;
+    }
+    if (!words[right]) {
+      right--;
+      continue;
+    }
+
+    const swap = words[left];
+    words[left] = words[right];
+    words[right] = swap;
+
+    left++;
+    right--;
+  }
+
+  return words.join(' ');
+};
+
+// 2023/04
 // Manually
 // O(n) time complexity
 // O(n) space complexity
