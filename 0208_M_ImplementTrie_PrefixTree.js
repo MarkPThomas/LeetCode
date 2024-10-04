8feb4a6260d14fff64a660148609483fc7832cbf
Mark P Thomas <markums@gmail.com>
2024-10-04 11:06:38 -0700

    Solved for 2024

--- 0208_M_ImplementTrie_PrefixTree.js
+++ 0208_M_ImplementTrie_PrefixTree.js
@@ -1,8 +1,70 @@
 // 2024/10/04
-// Time to complete:  min
+// Time to complete: 10:03  min
 // Patterns: Trie
 // Notes w.r.t. solution:
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isWord = false;
+  }
+}
 
+var Trie = function () {
+  this.root = new TrieNode();
+};
+
+/**
+* @param {string} word
+* @return {void}
+*/
+Trie.prototype.insert = function (word) {
+  let node = this.root;
+  for (let i = 0; i < word.length; i++) {
+    if (!node.children[word[i]]) {
+      node.children[word[i]] = new TrieNode();
+    }
+    node = node.children[word[i]];
+  }
+  node.isWord = true;
+};
+
+/**
+* @param {string} word
+* @return {boolean}
+*/
+Trie.prototype.search = function (word) {
+  let node = this.root;
+  for (let i = 0; i < word.length; i++) {
+    if (!node.children[word[i]]) {
+      return false;
+    }
+    node = node.children[word[i]];
+  }
+  return node.isWord;
+};
+
+/**
+* @param {string} prefix
+* @return {boolean}
+*/
+Trie.prototype.startsWith = function (prefix) {
+  let node = this.root;
+  for (let i = 0; i < prefix.length; i++) {
+    if (!node.children[prefix[i]]) {
+      return false;
+    }
+    node = node.children[prefix[i]];
+  }
+  return true;
+};
+
+/**
+* Your Trie object will be instantiated and called as such:
+* var obj = new Trie()
+* obj.insert(word)
+* var param_2 = obj.search(word)
+* var param_3 = obj.startsWith(prefix)
+*/
 
 
 // 2023/08
--- 0394_M_DecodeString.js
+++ 0394_M_DecodeString.js
@@ -1,6 +1,48 @@
+// 2024/10/04
 // O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// O() space complexity
+// Time to complete: Over time
+// Patterns: Stack
+// Notes w.r.t. solution: Was on the right track, could have done better diagramming out strategy more carefully before coding.
+//    People consider this one Hard.
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+  const stack = [];
 
+  let i = 0;
+  while (i < s.length) {
+    if (s[i] !== ']') {
+      stack.push(s[i]);
+      i++;
+    } else {
+      // Skip char
+      i++;
+
+      // Build string
+      let string = '';
+      let item = stack.pop();
+      while (item !== '[' && stack.length) {
+        string = item + string;
+        item = stack.pop();
+      }
+
+      // Build count
+      let kChar = '';
+      while (!isNaN(parseInt(stack[stack.length - 1])) && stack.length) {
+        kChar = stack.pop() + kChar;
+      }
+
+      // Add count to stack
+      let k = parseInt(kChar);
+      while (k) {
+        stack.push(string);
+        k--;
+      }
+    }
+  }
+
+  return stack.join('');
+};
