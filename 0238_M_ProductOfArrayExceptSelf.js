110d187d1c712ddb6258b9f9e10c9245c1d590f4
Mark P Thomas <markums@gmail.com>
2024-10-08 08:01:56 -0700

    Soved for 2024

--- 0238_M_ProductOfArrayExceptSelf.js
+++ 0238_M_ProductOfArrayExceptSelf.js
@@ -1,7 +1,41 @@
+// 2024/10/07
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 27:06 min
+// Patterns: Array/string, prefix
+// Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const prefixes = [];
+  for (let i = 0; i < nums.length; i++) {
+    const prefix = i > 0 ? prefixes[i - 1] : 1;
+    prefixes[i] = nums[i] * prefix;
+  }
+
+  const suffixes = [];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const suffix = i < nums.length - 1 ? suffixes[i + 1] : 1;
+    suffixes[i] = nums[i] * suffix;
+  }
+
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    const prefix = i > 0 ? prefixes[i - 1] : 1;
+    const suffix = i < nums.length - 1 ? suffixes[i + 1] : 1;
+    result.push(prefix * suffix);
+  }
+
+  return result;
+};
+
+// 2023/06
 // O(n) time complexity
 // O(1) space complexity
 // Time to complete: 27 min + 3 min (for optimization)
-// Patterns: Array/string.
+// Patterns: Array/string, prefix
 // Notes w.r.t. solution: Took a 'hail mary' hint about 15 min in, but worked out the solution from there pretty fast.
 
 /**
