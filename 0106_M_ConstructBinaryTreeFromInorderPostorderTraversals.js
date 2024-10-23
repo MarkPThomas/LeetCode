670a5db85998d5b67ed6c36b84baed92249a9ee6
Mark P Thomas <markums@gmail.com>
2024-10-23 13:54:59 -0600

    Re-solved for 2024

--- 0106_M_ConstructBinaryTreeFromInorderPostorderTraversals.js
+++ 0106_M_ConstructBinaryTreeFromInorderPostorderTraversals.js
@@ -1,7 +1,51 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min  9:51-
-// Patterns:
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 32:52 min
+// Patterns: Binary Tree, DFS Postorder, DFS Inorder
 // Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
 
-// Sad Panda DNF. Redo after learning algorithm. :-(
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function (inorder, postorder) {
+  const inOrderMap = {};
+  for (let i = 0; i < inorder.length; i++) {
+    inOrderMap[inorder[i]] = i;
+  }
+
+  let postIdx = postorder.length - 1;
+
+  function buildChildren(leftIdx, rightIdx) {
+    // At leaf
+    if (leftIdx > rightIdx) {
+      return null;
+    }
+
+    const rootVal = postorder[postIdx];
+    const root = new TreeNode(rootVal);
+
+    // Inorder root
+    const rootIdx = inOrderMap[rootVal];
+
+    // Increment postIdx backwards & build children in reverse
+    postIdx--;
+    root.right = buildChildren(rootIdx + 1, rightIdx);
+    root.left = buildChildren(leftIdx, rootIdx - 1);
+
+    return root;
+  }
+
+  let leftIdx = 0;
+  let rightIdx = inorder.length - 1;
+  return buildChildren(leftIdx, rightIdx, postIdx);
+}
