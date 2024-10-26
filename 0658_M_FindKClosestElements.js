e44a08c5feada8b385d4793827d1d190cb37857d
Mark P Thomas <markums@gmail.com>
2024-10-26 15:26:18 -0600

    Completed for 2024

--- 0658_M_FindKClosestElements.js
+++ 0658_M_FindKClosestElements.js
@@ -1,17 +1,74 @@
-// O() time complexity
+// O(k + log(n)) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+//  where k = #s sought, n = arr size
+// Time to complete: Timeout - 20:55 min to solve 99% (55/70)
+// Patterns: Binary Search
+// Notes w.r.t. solution: 20:55 min to solve 99% (55/70). Went OT working out details of incrementing left/right. Should have slowed down & diagrammed that part out.
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+  function aIsCloser(a, b, x) {
+    return Math.abs(a - x) < Math.abs(b - x)
+      || (a < b && Math.abs(a - x) === Math.abs(b - x));
   }
-);
+
+  // Simplest cases
+  if (arr.length === k) {
+    return arr;
+  } else if (x <= arr[0]) {
+    return arr.slice(0, k);
+  } else if (arr[arr.length - 1] <= x) {
+    return arr.slice(arr.length - k);
+  }
+
+  // Find x
+  let idx = -1;
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (arr[mid] === x) {
+      idx = mid;
+      break;
+    } else if (arr[mid] < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  // If x not found, find closest #
+  if (idx === -1) {
+    if (aIsCloser(arr[left], arr[right], x)) {
+      idx = left;
+    } else {
+      idx = right;
+    }
+  }
+
+  if (k === 1) {
+    return [arr[idx]];
+  }
+
+  // Work outwards from x, L & R, adding closest to result
+  left = idx;
+  right = idx;
+  while (right - left < k - 1) {
+    const a = arr[left - 1];
+    const b = arr[right + 1];
+
+    if (left && (right === arr.length - 1 || aIsCloser(a, b, x))) { // a closer
+      left--;
+    } else { // b closer
+      right++;
+    }
+  }
+
+  return arr.slice(left, right + 1);
+};
--- 1171_M_RemoveZeroSumConsecutiveNodesFromLinkedList.js
+++ 1171_M_RemoveZeroSumConsecutiveNodesFromLinkedList.js
@@ -1,9 +1,117 @@
+// 2024/10/26
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: NA
+// Patterns: Linked List, Prefix sum, Hashmap
+// Notes w.r.t. solution: Worked out single pass where hashmap is updated for removals as we go
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeZeroSumSublists = function (head) {
+  const sentinel = new ListNode(null, head);
+
+  let visited = { 0: sentinel }
+  let node = sentinel.next;
+  let sum = 0;
+
+  while (node) {
+    sum += node.val;
+
+    if (visited.hasOwnProperty(sum)) {
+      let remove = null;
+      let repeat = visited[sum].next;
+
+      // Detach sequence & advance past
+      visited[sum].next = node.next;
+      node.next = null;
+      node = visited[sum].next;
+
+      // Remove sequence from hash table
+      const triggerSum = sum;
+      while (repeat) {
+        sum += repeat.val;
+        remove = repeat;
+        repeat = repeat.next;
+
+        if (sum && sum !== triggerSum) {
+          delete visited[sum];
+        }
+      }
+    } else {
+      visited[sum] = node;
+      node = node.next;
+    }
+  }
+
+  return sentinel.next;
+};
+
+
+// 2024/10/26
+// O(m * n) -> O(n) time complexity
+// O(n) space complexity
+//  where n = # nodes, m = # passes on LL, which is < n & decreasing with each pass, so , n < m * n < n^2
+// Time to complete: 29:31 (22:00)
+// Patterns: Linked List, Prefix sum, Hashmap
+// Notes w.r.t. solution: Mostly solved in 21:00.
+//  Had minor console.log read bug that took time to work out.
+//  Remainder of tweaks took about a minute.
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeZeroSumSublists = function (head) {
+  const sentinel = new ListNode(null, head);
+
+  let remove = false;
+  do {
+    let visited = { 0: sentinel }
+    let node = sentinel.next;
+    let sum = 0;
+    remove = false;
+
+    while (node) {
+      sum += node.val;
+
+      if (visited.hasOwnProperty(sum)) {
+        remove = true;
+
+        visited[sum].next = node.next;
+        node.next = null;
+
+        break;
+      } else {
+        visited[sum] = node;
+        node = node.next;
+      }
+    }
+  } while (remove)
+
+  return sentinel.next;
+};
+
+
+// 2023/04
 // O() time complexity
 // O(1) space complexity
 // Time to complete: xx min 11:19-
 // Patterns:
 // Notes w.r.t. solution:
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
