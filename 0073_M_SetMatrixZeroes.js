d4649d969a0109895558a177d8a7f9db61c020b9
Mark P Thomas <markums@gmail.com>
2024-11-11 10:35:33 -0700

    Solved for 2024

--- 0073_M_SetMatrixZeroes.js
+++ 0073_M_SetMatrixZeroes.js
@@ -1,17 +1,103 @@
-// O() time complexity
+// 2024/11/11
+// O(m * n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
+// Time to complete: 26:133 min
 // Patterns:
 // Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  let baseRowHasZero = false;
+  for (let row = 0; row < matrix.length; row++) {
+    if (matrix[row][0] === 0) {
+      baseRowHasZero = true;
+      break;
+    }
   }
-);
+
+  let baseColHasZero = false;
+  for (let col = 0; col < matrix[0].length; col++) {
+    if (matrix[0][col] === 0) {
+      baseColHasZero = true;
+      break;
+    }
+  }
+
+  // Mark outer edges
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        matrix[row][0] = 0;
+        matrix[0][col] = 0;
+      }
+    }
+  }
+
+  // 2nd pass transfers edge marks
+  for (let row = 1; row < matrix.length; row++) {
+    if (matrix[row][0] === 0) {
+      for (let col = 1; col < matrix[0].length; col++) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  for (let col = 1; col < matrix[0].length; col++) {
+    if (matrix[0][col] === 0) {
+      for (let row = 1; row < matrix.length; row++) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  // Update edges last
+  if (baseRowHasZero) {
+    for (let row = 0; row < matrix.length; row++) {
+      matrix[row][0] = 0;
+    }
+  }
+
+  if (baseColHasZero) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      matrix[0][col] = 0;
+    }
+  }
+};
+
+// 2024/11/11
+// O(m * n) time complexity
+// O(m + n) space complexity
+// Time to complete: 5:03 min
+// Patterns: Hashmap
+// Notes w.r.t. solution:
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  const rowsZero = {};
+  const colsZero = {};
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        rowsZero[row] = true;
+        colsZero[col] = true;
+      }
+    }
+  }
+
+  for (const row of Object.keys(rowsZero)) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      matrix[row][col] = 0;
+    }
+  }
+
+  for (const col of Object.keys(colsZero)) {
+    for (let row = 0; row < matrix.length; row++) {
+      matrix[row][col] = 0;
+    }
+  }
+};
