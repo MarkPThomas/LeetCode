e55ed7f288d7216f7cad5e70ae9b6f6b5d051237
Mark P Thomas <markums@gmail.com>
2024-11-20 11:40:20 -0700

    Solved for 2024

--- 0841_M_KeysAndRooms.js
+++ 0841_M_KeysAndRooms.js
@@ -1,6 +1,29 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/11/20
+// O(n + e) time complexity
+// O(n) space complexity
+// where n = # rooms,
+//    e = # edges between rooms
+// Time to complete: 8:25 min
+// Patterns: Graph DFS
 // Notes w.r.t. solution:
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function (rooms) {
+  const visited = {};
+  const stack = [0];
+  while (queue.length) {
+    const room = stack.pop();
+    const keys = rooms[room];
+    for (const key of keys) {
+      if (!visited[key]) {
+        stack.push(key);
+      }
+    }
 
+    visited[room] = true;
+  }
+
+  return Object.keys(visited).length === rooms.length;
+};
