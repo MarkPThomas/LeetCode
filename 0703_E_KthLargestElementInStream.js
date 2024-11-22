5c22b4f7495b4a4ac0b4fea5a8abab2645662719
Mark P Thomas <markums@gmail.com>
2024-11-22 14:56:47 -0700

    Reworked for 2024

--- 0703_E_KthLargestElementInStream.js
+++ 0703_E_KthLargestElementInStream.js
@@ -1,3 +1,179 @@
+// 2024/11/22
+// O(m * n * log(n)) time complexity
+// O(n) space complexity
+// where n = length of initial array, m = # items added later, k = k as in top k defined below
+// Time to complete:
+// Patterns: Sorting
+// Notes w.r.t. solution: Brute force, naiive solution
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  this.sizeLimit = k;
+
+  this.nums = nums;
+  this.nums.sort((a, b) => a - b);
+  if (this.nums.length > this.sizeLimit) {
+    this.nums = this.nums.slice(this.nums.length - this.sizeLimit);
+  }
+};
+
+/**
+* @param {number} val
+* @return {number}
+*/
+KthLargest.prototype.add = function (val) {
+  if (this.nums.length === this.sizeLimit && this.nums[0] > val) {
+    // Don't add smaller values to full kth largest
+    return this.nums[0];
+  }
+
+  this.nums.push(val);
+  this.nums.sort((a, b) => a - b);
+  if (this.nums.length > this.sizeLimit) {
+    this.nums.shift();
+  }
+
+  return this.nums[0];
+};
+
+
+// 2024/11/22
+// O(n*log(n) + m * log(k)) time complexity
+// O(1) space complexity
+// where n = length of initial array, m = # items added later, k = k as in top k defined below
+// Time to complete: OT - 25:30 to write solution. Minor issues took too long to debug.
+// Patterns: Heap
+// Notes w.r.t. solution: Cleaner code separations w/ purer heap in this round.
+//  When working at odds w/ heaps (e.g. largest k in a min heap), keep size limitations or logic on how to add
+//    values at odds in a containing class and NOT within the Heap class.
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  this.sizeLimit = k;
+  this.minHeap = new MinHeap(nums);
+
+  while (this.minHeap.nums.length > this.sizeLimit) {
+    this.minHeap.removeRoot();
+  }
+};
+
+/**
+* @param {number} val
+* @return {number}
+*/
+KthLargest.prototype.add = function (val) {
+  if (this.minHeap.nums.length === this.sizeLimit) {
+    if (this.minHeap.peek() > val) {
+      // Don't add smaller values to full kth largest
+      return this.minHeap.peek();
+    } else {
+      // Remove smallest value before adding
+      this.minHeap.removeRoot();
+    }
+  }
+
+  this.minHeap.insert(val);
+
+  return this.minHeap.peek();
+};
+
+/**
+* Your KthLargest object will be instantiated and called as such:
+* var obj = new KthLargest(k, nums)
+* var param_1 = obj.add(val)
+*/
+
+class MinHeap {
+  constructor(nums) {
+    this.nums = nums;
+    this.build();
+  }
+
+  build() {
+    for (let i = Math.floor(this.nums.length / 2) - 1; i >= 0; i--) {
+      this.heapifyDown(i);
+    }
+  }
+
+  peek() {
+    return this.nums[0];
+  }
+
+  insert(val) {
+    if (this.nums.length === this.sizeLimit) {
+      if (this.nums[0] > val) {
+        return;
+      } else {
+        this.nums[0] = val;
+        this.heapifyDown(0);
+      }
+    } else {
+      this.nums.push(val);
+      this.heapifyUp(this.nums.length - 1);
+    }
+  }
+
+  removeRoot() {
+    this.swap(0, this.nums.length - 1);
+    this.nums.pop();
+
+    this.heapifyDown(0);
+  }
+
+  heapifyUp(targetIndex) {
+    while (targetIndex) {
+      const parentIndex = this.getParentIndex(targetIndex);
+      if (this.shouldSwap(targetIndex, parentIndex)) {
+        this.swap(targetIndex, parentIndex);
+        targetIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  heapifyDown(targetIndex) {
+    while (targetIndex < this.nums.length) {
+      let swapIndex = targetIndex;
+      swapIndex = this.getSwapIndex(1, targetIndex, swapIndex);
+      swapIndex = this.getSwapIndex(2, targetIndex, swapIndex);
+
+      if (swapIndex === targetIndex) {
+        break;
+      }
+
+      this.swap(swapIndex, targetIndex);
+      targetIndex = swapIndex;
+    }
+  }
+
+  getSwapIndex(childNum, targetIndex, swapIndex) {
+    const childIdx = targetIndex * 2 + childNum;
+
+    return childIdx < this.nums.length
+      && this.shouldSwap(childIdx, swapIndex) ? childIdx : swapIndex;
+  }
+
+  shouldSwap(childIdx, parentIdx) {
+    return this.nums[childIdx] < this.nums[parentIdx];
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  swap(i, j) {
+    const temp = this.nums[i];
+    this.nums[i] = this.nums[j];
+    this.nums[j] = temp;
+  }
+}
+
+// 2023/05
 // O(n*log(n) + m*log(k)) time complexity
 // O(1) space complexity
 // where n = length of initial array, m = # items added later, k = k as in top k defined below
