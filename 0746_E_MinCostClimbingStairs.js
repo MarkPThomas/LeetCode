d2cdac381d2bf90a10fd7fd40ec0bbed09a61c7c
Mark P Thomas <markums@gmail.com>
2024-12-09 12:34:42 -0700

    Resolved for 2024, including reduced state

--- 0746_E_MinCostClimbingStairs.js
+++ 0746_E_MinCostClimbingStairs.js
@@ -1,3 +1,49 @@
+// 2024/12/09
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 28:35 min
+// Patterns: DP, Top-down/tabulation/interative, State Reduction
+// Notes w.r.t. solution: Time is total to prev solution & then refactoring state reduction.
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  let prevStair2 = 0;
+  let prevStair1 = 0;
+
+  for (let i = 0; i < cost.length; i++) {
+    const currStair = cost[i] + Math.min(prevStair1, prevStair2);
+    prevStair2 = prevStair1;
+    prevStair1 = currStair;
+  }
+
+  return Math.min(prevStair1, prevStair2);
+};
+
+
+// 2024/12/09
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 25:53 min
+// Patterns: DP, Top-down/tabulation/interative
+// Notes w.r.t. solution: Lost time not thinking carefully at first.
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  const minCosts = Array(cost.length).fill(Infinity);
+  minCosts[0] = cost[0];
+  minCosts[1] = cost[1];
+
+  for (let i = 0; i < cost.length; i++) {
+    minCosts[i] = cost[i] + Math.min(minCosts[i - 1], minCosts[i - 2]);
+  }
+
+  return Math.min(minCosts[minCosts.length - 1], minCosts[minCosts.length - 2]);
+};
+
 // 2024/09/22
 // O(n) time complexity
 // O(n) space complexity
