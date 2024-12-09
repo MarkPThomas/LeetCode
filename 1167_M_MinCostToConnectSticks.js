d395cee7f284d21e121615012a9a0362326b444e
Mark P Thomas <markums@gmail.com>
2024-12-09 12:34:23 -0700

    Solved for 2024

--- 1167_M_MinCostToConnectSticks.js
+++ 1167_M_MinCostToConnectSticks.js
@@ -1,6 +1,27 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/12/09
+// O(n * log(n)) time complexity
+// O(n) space complexity
+// Time to complete: 13:00 min
+// Patterns: Greedy, Priority Heap
 // Notes w.r.t. solution:
+/**
+ * @param {number[]} sticks
+ * @return {number}
+ */
+var connectSticks = function (sticks) {
+  let joinedSticks = new PriorityQueue({ compare: (a, b) => a - b });
+  for (const stick of sticks) {
+    joinedSticks.enqueue(stick);
+  }
 
+  let totalCost = 0;
+  while (joinedSticks.size() > 1) {
+    let sum = joinedSticks.dequeue();
+    sum += joinedSticks.dequeue();
+    totalCost += sum;
+
+    joinedSticks.enqueue(sum);
+  }
+
+  return totalCost;
+};
