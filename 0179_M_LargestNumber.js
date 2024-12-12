9d2a7aaf4664d9e165734e493798ca3a8dce0643
Mark P Thomas <markums@gmail.com>
2024-12-12 10:39:07 -0700

    Minor comment tweak

--- 0179_M_LargestNumber.js
+++ 0179_M_LargestNumber.js
@@ -3,7 +3,7 @@
 // O(n) space complexity
 // Time to complete: 20:00 min
 // Patterns: Greedy
-// Notes w.r.t. solution: Should have thought through the comparator more carefully;
+// Notes w.r.t. solution: Should have thought through the comparator more carefully.
 /**
  * @param {number[]} nums
  * @return {string}
