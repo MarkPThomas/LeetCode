016b12bfee47f4001eb42aa32e4658aa0207a9f1
Mark P Thomas <markums@gmail.com>
2024-12-12 10:38:51 -0700

    Solved 2024

--- 0179_M_LargestNumber.js
+++ 0179_M_LargestNumber.js
@@ -1,17 +1,18 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+// 2024/12/12
+// O(n * log(n)) time complexity
+// O(n) space complexity
+// Time to complete: 20:00 min
+// Patterns: Greedy
+// Notes w.r.t. solution: Should have thought through the comparator more carefully;
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i].toString();
   }
-);
+  nums.sort((a, b) => (b + a) - (a + b));
+
+  return nums[0] === '0' ? '0' : nums.join('');
+};
