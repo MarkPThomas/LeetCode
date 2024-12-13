1c5ac2506e074e83193c6b2df717845922f4a78b
Mark P Thomas <markums@gmail.com>
2024-12-13 14:47:49 -0700

    Solved 2024

--- 0023_H_MergeKSortedLists.js
+++ 0023_H_MergeKSortedLists.js
@@ -1,5 +1,42 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2024/12/12
+// O(n * log(k)) time complexity
+// O(k) space complexity
+// where n = # nodes, k = # linked lists
+// Time to complete: 6:49 min
+// Patterns: Priority Queue, Linked List
 // Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  const pq = new PriorityQueue({ compare: (a, b) => a.val - b.val });
+
+  for (const list of lists) {
+    if (list) {
+      pq.enqueue(list);
+    }
+  }
+
+  const sentinel = new ListNode();
+  let tail = sentinel;
+  while (pq.size()) {
+    const node = pq.dequeue();
+
+    tail.next = node;
+    tail = node;
+
+    if (node.next) {
+      pq.enqueue(node.next);
+    }
+  }
+
+  return sentinel.next;
+};
