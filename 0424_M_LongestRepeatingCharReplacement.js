1d9b4e4cfcfcbaf4c19a779be3b70dd30a9ffb14
Mark P Thomas <markums@gmail.com>
2024-12-13 14:48:06 -0700

    Attempted Dec 2024

--- 0424_M_LongestRepeatingCharReplacement.js
+++ 0424_M_LongestRepeatingCharReplacement.js
@@ -1,17 +1,46 @@
+// 2024/12/13
 // O() time complexity
 // O(1) space complexity
-// Time to complete: xx min
+// Time to complete: OT min 41/49.
 // Patterns:
-// Notes w.r.t. solution:
+// Notes w.r.t. solution: Window needed to shrink the left size.
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+  let maxCount = 0;
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  let startIdx = 0;
+  let endIdx = 0;
+  let nextStartIdx = 0;
+  let kCurr = k;
+  while (endIdx < s.length) {
+    if (s[startIdx] === s[endIdx]) {
+      // Grow window forward
+      endIdx++;
+    } else {
+      if (!nextStartIdx) {
+        nextStartIdx = endIdx;
+      }
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+      if (kCurr) {
+        // Grow window forward by at most k remaining
+        kCurr--;
+        endIdx++
+      } else {
+        // Take count, start over at last letter change w/ reset k
+        maxCount = Math.max(maxCount, endIdx - startIdx);
+
+        startIdx = nextStartIdx;
+        nextStartIdx = 0;
+        endIdx = startIdx;
+        kCurr = k;
+      }
+    }
   }
-);
+  maxCount = Math.max(maxCount, endIdx - startIdx);
+
+  return maxCount;
+}
