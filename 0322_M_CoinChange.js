210bd856dc8e484d8cb79ebf58eaaeafa00f3401
Mark P Thomas <markums@gmail.com>
2024-12-17 11:14:36 -0700

    Solved for 2024

--- 0322_M_CoinChange.js
+++ 0322_M_CoinChange.js
@@ -1,17 +1,55 @@
+// 2024/12/16
+// O(t * c) time complexity
+// O(t) space complexity
+//  where t = total, c = # coins
+// Time to complete: 37:21 min / 48:32 min
+// Patterns: Dynamic Programming w/ Iteration
+// Notes w.r.t. solution: Solved in 37:21, but optimized until 48:32.
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, total) {
+  const dp = Array(total + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let currTotal = 1; currTotal <= total; currTotal++) {
+    for (const coin of coins) {
+      if (coin <= currTotal) {
+        dp[currTotal] = Math.min(dp[currTotal], dp[currTotal - coin] + 1);
+      }
+    }
+  }
+
+  return dp[total] === Infinity ? -1 : dp[total];
+};
+
+
 // O() time complexity
 // O(1) space complexity
 // Time to complete: xx min
 // Patterns:
-// Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+// Notes w.r.t. solution: Hack Reactor solution?
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, total) {
+  if (total < 0) {
+    return -1;
   }
-);
+
+  const counter = Array(total + 1).fill(Infinity);
+  counter[0] = 0;
+
+  for (let subTotal = 1; subTotal <= total; subTotal++) {
+    coins.forEach((coinValue) => {
+      if (coinValue <= subTotal) {
+        counter[subTotal] = Math.min(counter[subTotal], counter[subTotal - coinValue] + 1);
+      }
+    });
+  }
+  return counter[total] > total ? -1 : counter[total];
+};
--- 0417_M_PacificAtlanticWaterFlow.js
+++ 0417_M_PacificAtlanticWaterFlow.js
@@ -1,17 +1,74 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/12/16
+// O(m * n) time complexity
+// O(m * n) space complexity
+// Time to complete: 18:37/OT min
+// Patterns: BFS
+// Notes w.r.t. solution: I went to OT debugging.
+//  I was close, but missed using a 2D 'reachable' matrix instead of visited hashmaps + overlapping leaf nodes.
+//  Reading the problem a bit closer (reachable cells) & starting a tad slower would have helped.
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function (heights) {
+  const DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  function getNeighbors([row, col], reachable, neighbors) {
+    for (const [deltaRow, deltaCol] of DIRS) {
+      const nextRow = row + deltaRow;
+      const nextCol = col + deltaCol;
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+      if (nextRow < 0 || heights.length <= nextRow
+        || nextCol < 0 || heights[0].length <= nextCol) {
+        continue;
+      }
+
+      if (!reachable[nextRow][nextCol] && heights[nextRow][nextCol] >= heights[row][col]) {
+        neighbors.push([nextRow, nextCol]);
+      }
+    }
   }
-);
+
+  function getReachable(cells) {
+    const reachable = Array(heights.length).fill().map(() => Array(heights[0].length).fill(false));
+
+    while (cells.length) {
+      const nextCells = [];
+      for (let i = 0; i < cells.length; i++) {
+        reachable[cells[i][0]][cells[i][1]] = true;
+        getNeighbors(cells[i], reachable, nextCells);
+      }
+
+      cells = nextCells;
+    }
+
+    return reachable;
+  }
+
+  const pacific = [];
+  const atlantic = [];
+
+  for (let row = 0; row < heights.length; row++) {
+    pacific.push([row, 0]);
+    atlantic.push([row, heights[0].length - 1]);
+  }
+
+  for (let col = 0; col < heights[0].length; col++) {
+    pacific.push([0, col]);
+    atlantic.push([heights.length - 1, col]);
+  }
+
+  const pacificReachable = getReachable(pacific);
+  const atlanticReachable = getReachable(atlantic);
+
+  const common = []
+  for (let row = 0; row < heights.length; row++) {
+    for (let col = 0; col < heights[0].length; col++) {
+      if (pacificReachable[row][col] && atlanticReachable[row][col]) {
+        common.push([row, col]);
+      }
+    }
+  }
+
+  return common;
+};
