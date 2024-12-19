ed5dcd464b4230688c8f916aaf17cc5566c3ca79
Mark P Thomas <markums@gmail.com>
2024-12-19 14:43:59 -0700

    Solved for 2024

--- 0529_M_Minesweeper.js
+++ 0529_M_Minesweeper.js
@@ -1,17 +1,61 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// O(m * n) time complexity
+// O(m * n) space complexity
+// Time to complete: OT min
+// Patterns: BFS
+// Notes w.r.t. solution: Lost time initially marking squares IP rather than using an auxillary array for visited.
+//    Also, for some reason got TLE. Example solution had minor rearrangement that passed in time.
+/**
+ * @param {character[][]} board
+ * @param {number[]} click
+ * @return {character[][]}
+ */
+var updateBoard = function (board, click) {
+  const DIRS = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  const [rowClick, colClick] = click;
+  const clickValue = board[rowClick][colClick];
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+  if (clickValue === 'M') {
+    board[rowClick][colClick] = 'X';
+  } else if (clickValue === 'E') {
+    const visited = Array(board.length).fill().map(() => Array(board[0].length).fill(false));
+
+    let queue = [click];
+    while (queue.length) {
+      const [row, col] = queue.pop();
+
+      let mines = 0;
+      let adjCells = [];
+      for (const [deltRow, deltCol] of DIRS) {
+        const adjRow = row + deltRow;
+        const adjCol = col + deltCol;
+
+        if (adjRow < 0 || board.length <= adjRow
+          || adjCol < 0 || board[0].length <= adjCol) {
+          continue;
+        }
+
+        if (board[adjRow][adjCol] === 'M') {
+          mines++;
+        } else if (board[adjRow][adjCol] === 'E') {
+          adjCells.push([adjRow, adjCol]);
+        }
+      }
+
+      if (mines) {
+        board[row][col] = mines.toString();
+      } else {
+        board[row][col] = 'B';
+
+        for (const [adjRow, adjCol] of adjCells) {
+          if (!visited[adjRow][adjCol]) {
+            queue.push(...adjCells);
+            visited[adjRow][adjCol] = true;
+          }
+        }
+      }
+    }
   }
-);
+
+  return board;
+};
