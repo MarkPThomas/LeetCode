2439f1a4668672dc357f26fde819595ec718d592
Mark P Thomas <markums@gmail.com>
2024-12-19 11:39:22 -0700

    Solved 2024

--- 0300_M_LongestIncreasingSubsequence.js
+++ 0300_M_LongestIncreasingSubsequence.js
@@ -1,17 +1,62 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/12/17
+// O(n^2) time complexity
+// O(n) space complexity
+// Time to complete: NA min
+// Patterns: DP - Iteration
+// Notes w.r.t. solution: After peeking at solution, making tweaks to my prior attempt
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+// 2024/12/17
+// O(n^3 ?) time complexity
+// O(n) space complexity
+// Time to complete: OT min
+// Patterns: DP - Iteration
+// Notes w.r.t. solution: Failed attempt. Got up to solving 24/55.
+//  I was on the right track, but got bogged down/rushed in adding additional limits & loops rather than
+//    seeing how to consolidate them into how 2 nested loops can work.
+//  So close!
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let maxLength = 1;
+  for (start = 0; start < nums.length - maxLength; start++) {
+    let maxIdx = start;
+    const dp = Array(nums.length).fill(1);
+
+    while (nums.length - maxIdx + 1 >= maxLength) {
+      let length = dp[maxIdx];
+      let currMaxIdx = maxIdx;
+      let nextMaxIdx = 0;
+      for (i = maxIdx + 1; i < nums.length; i++) {
+        if (nums[currMaxIdx] < nums[i]) {
+          if (!nextMaxIdx) {
+            nextMaxIdx = i;
+          }
+          currMaxIdx = i;
+          length++;
+        }
+        dp[i] = length;
+      }
+      maxLength = Math.max(maxLength, length);
+
+      // see if another maxIdx is better
+      while (nextMaxIdx < nums.length) {
+        if (nums[start] < nums[nextMaxIdx] && nums[nextMaxIdx] < nums[maxIdx]) {
+          break;
+        }
+        nextMaxIdx++;
+      }
+
+      if (nextMaxIdx === maxIdx || nextMaxIdx === nums.length) {
+        // Next max id is unchanged or out of bounds
+        break;
+      } else {
+        maxIdx = nextMaxIdx;
+      }
+    }
   }
-);
+
+  return maxLength;
+};
--- 0525_M_ContiguousArray.js
+++ 0525_M_ContiguousArray.js
@@ -1,17 +1,33 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2024/12/18
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 12:03/15:57 min
+// Patterns: Hashmap
+// Notes w.r.t. solution: 2nd time was total time including for optimization beyond first solving @ 12:03
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function (nums) {
+  let count = 0;
+  let maxLength = 0;
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  const counts = {};
+  counts[0] = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      count--;
+    } else {
+      count++;
+    }
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+    if (count in counts) {
+      const length = i - counts[count];
+      maxLength = Math.max(maxLength, length);
+    } else {
+      counts[count] = i;
+    }
   }
-);
+
+  return maxLength;
+};
