5b3823f7b25bc715a8db9e9b097b8637c787b0ae
Mark P Thomas <markums@gmail.com>
2024-12-28 12:51:44 -0700

    Solved 2024

--- 0048_M_RotateImage.js
+++ 0048_M_RotateImage.js
@@ -1,17 +1,31 @@
-// O() time complexity
+// 2024/12/27
+// O(m * n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
+// Time to complete: 11:08 min
 // Patterns:
 // Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  // Transpose
+  for (let row = 1; row < matrix.length; row++) {
+    for (let col = 0; col < row; col++) {
+      const swap = matrix[row][col];
+      matrix[row][col] = matrix[col][row];
+      matrix[col][row] = swap;
+    }
   }
-);
+
+  // Reflect
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < Math.floor(matrix[0].length / 2); col++) {
+      const swap = matrix[row][col];
+      const colMirror = matrix[0].length - col - 1;
+
+      matrix[row][col] = matrix[row][colMirror];
+      matrix[row][colMirror] = swap;
+    }
+  }
+};
--- 0518_M_CoinChangeII.js
+++ 0518_M_CoinChangeII.js
@@ -1,6 +1,110 @@
+// 2024/12/27
+// O(c * n) time complexity
+// O(n) space complexity
+//  where n = amount, c = # coins
+// Time to complete: OT min
+// Patterns: Dynamic Programming (Iteration, Counting)
+// Notes w.r.t. solution: Looked at answer :-P
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  coins.sort((a, b) => a - b);
+
+  const combos = Array(amount + 1).fill(0);
+  combos[0] = 1;
+
+  for (const coin of coins) {
+    for (let sum = coin; sum <= amount; sum++) {
+      combos[sum] += combos[sum - coin];
+    }
+  }
+
+  return combos[amount];
+};
+
+// 2024/12/27
+// O() time complexity
+// O(1) space complexity
+// Time to complete: OT min
+// Patterns: Dynamic Programming (Iteration, Counting)
+// Notes w.r.t. solution: I was close! Incomplete solution below.
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  coins.sort((a, b) => a - b);
+
+  const combos = Array(amount + 1).fill(0);
+  combos[0] = 1;
+
+  for (let sum = 1; sum <= amount; sum++) {
+    let ways = 0;
+    for (let i = 0; i < coins.length; i++) {
+      if (coins[i] > sum) {
+        break;
+      }
+
+      ways = Math.max(ways, combos[sum - coins[i]]);
+
+      // if (sum % coins[i] === 0) {
+      if (coins[i] === sum) {
+        ways++;
+      }
+    }
+
+    // for (let i = 0; i < coins.length; i++) {
+    //     if (coins[i] > sum) {
+    //         break;
+    //     }
+
+    //     // if (sum - coins[i] === 0) {
+    //     //     ways++;
+    //     // }
+
+    //     let maxWays = 0;
+    //     for (let j = i; j >= 0; j--) {
+    //         // maxWays += combos[sum - coins[j]];
+    //         maxWays = Math.max(maxWays, combos[sum - coins[j]]);
+    //     }
+
+    //     ways += maxWays;
+    //     // ways = maxWays;
+    // }
+    combos[sum] = ways;
+  }
+
+  return combos[amount];
+};
+
+
+// 2021/??/??
 // O() time complexity
 // O(1) space complexity
 // Time to complete: xx min
 // Patterns:
 // Notes w.r.t. solution:
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  if (amount < 0) {
+    return 0;
+  }
 
+  const counter = Array(amount + 1).fill(0);
+  counter[0] = 1;
+
+  coins.forEach((coinValue) => {
+    for (let subTotal = coinValue; subTotal <= amount; subTotal++) {
+      counter[subTotal] += counter[subTotal - coinValue];
+    }
+  });
+  return counter[amount];
+};
