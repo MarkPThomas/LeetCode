565590014b6d64e8be2239182e0cd1cce1fe4c09
Mark P Thomas <markums@gmail.com>
2025-01-04 11:48:06 -0700

    Solved for 2025

--- 0424_M_LongestRepeatingCharReplacement.js
+++ 0424_M_LongestRepeatingCharReplacement.js
@@ -1,3 +1,51 @@
+// 2025/01/04
+// O(n) time complexity
+// O(m)->O(1) space complexity
+//  where n = length of string,
+//    m = # chars = constant 26 here
+// Time to complete: 10:45/13:32 min
+// Patterns: Sliding Window
+// Notes w.r.t. solution: Solved in 10:45. Optimized through 13:32 for counting maxFreq.
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+  // const charCounts = new PriorityQueue((a, b) => b[1] - a[1]);
+  const charCounts = {};
+  let maxLength = 0;
+  let maxFreq = 0;
+
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    charCounts[s[right]] ||= 0;
+    charCounts[s[right]]++;
+
+    // char to use is the one that occurs the most times
+    maxFreq = Math.max(maxFreq, charCounts[s[right]]);
+
+    // Window is valid if window length - charToUse freq <= k
+    const currLength = right - left + 1;
+    if (currLength - maxFreq <= k) {
+      maxLength = Math.max(maxLength, currLength);
+    } else {
+      charCounts[s[left]]--;
+      left++;
+      // Note: maxFreq is temporarily out of date by potentially being too large.
+      //  This is OK as it only matters when we potentially have a larger window.
+      //  By being too large, all that happes is:
+      //      1. We check maxLength more
+      //      2. We shrink the window less
+      //  This is corrected when we encounter the next increase in maxFreq.
+    }
+  }
+
+
+  return maxLength;
+}
+
+
 // 2024/12/13
 // O() time complexity
 // O(1) space complexity
