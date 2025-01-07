5eff5b0dc50efccab35b706c10448155fcab79bf
Mark P Thomas <markums@gmail.com>
2025-01-07 11:33:02 -0700

    Completed for 2025

--- 0199_M_BinaryTreeRightSideView.js
+++ 0199_M_BinaryTreeRightSideView.js
@@ -1,6 +1,44 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/01/07
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 3:53 min
+// Patterns: BFS
 // Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if (!root) {
+    return [];
+  }
 
+  // BFS, record last node in each layer
+  const rightMost = [];
+  let queue = [root];
+  while (queue.length) {
+    const nextQueue = [];
+    let node = null;
+    for (let i = 0; i < queue.length; i++) {
+      node = queue[i];
+      if (node.left) {
+        nextQueue.push(node.left);
+      }
+      if (node.right) {
+        nextQueue.push(node.right);
+      }
+    }
+    rightMost.push(node.val);
+    queue = nextQueue;
+  }
+
+  return rightMost;
+};
