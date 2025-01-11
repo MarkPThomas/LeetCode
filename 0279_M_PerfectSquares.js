81139ddc998f8e078c6079ebf573a2c6973efea7
Mark P Thomas <markums@gmail.com>
2025-01-11 15:46:43 -0700

    Solved for 2025

--- 0279_M_PerfectSquares.js
+++ 0279_M_PerfectSquares.js
@@ -1,7 +1,28 @@
-// yyyymmdd
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/01/11
+// O(n * SQRT(n)) time complexity
+// O(n) space complexity
+// Time to complete: 32:57 min
+// Patterns: DP - Iteration/Combination
 // Notes w.r.t. solution:
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function (n) {
+  const dp = Array(n + 1).fill(Infinity);
+  dp[0] = 0;
 
+  let num = 1;
+  while (num ** 2 <= n) {
+    const perfectNum = num ** 2;
+    for (let i = perfectNum; i <= n; i++) {
+      let count = Math.floor(i / perfectNum) + dp[i % perfectNum];
+
+      dp[i] = Math.min(dp[i], count);
+    }
+
+    num++;
+  }
+
+  return dp[n];
+};
