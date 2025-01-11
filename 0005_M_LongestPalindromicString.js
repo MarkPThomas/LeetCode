58242261325f0ecb7913c568db181ce04d06dd59
Mark P Thomas <markums@gmail.com>
2025-01-11 14:49:42 -0700

    Solved 2025

--- 0005_M_LongestPalindromicString.js
+++ 0005_M_LongestPalindromicString.js
@@ -1,3 +1,62 @@
+// 2025/01/11
+// O(n^2) time complexity
+// O(1) space complexity
+// Time to complete: 22:49 min
+// Patterns: 2 Pointers
+// Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  // is palindrome if mid - offset = mid + offset
+  // from base case, odd/even treated the same & palindrome check naturally grows out
+
+  // states: mid, offset
+  // base cases:
+  //  odd - check starts at mid = mid
+  //  even, check starts at mid = mid + 1
+  //  min palindrome = 1 for any char
+
+  function longestAt(mid, offset) {
+    let left = mid;
+    let right = mid + offset;
+
+    let length = 1;
+    if (s[left] !== s[right]) {
+      return [left, length];
+    }
+    length += offset;
+
+    while (0 <= left && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+      length += 2;
+    }
+
+    return [left + 1, length - 2];
+  }
+
+  let longest = 1;
+  let left = 0;
+
+  for (let mid = 0; mid < s.length; mid++) {
+    const [oddLeft, oddLength] = longestAt(mid, 0);
+    if (oddLength > longest) {
+      longest = oddLength;
+      left = oddLeft;
+    }
+
+    const [evenLeft, evenLength] = longestAt(mid, 1);
+    if (evenLength > longest) {
+      longest = evenLength;
+      left = evenLeft;
+    }
+  }
+
+  return s.substring(left, left + longest);
+};
+
 // 2025/01/05
 // O(n^2) time complexity
 // O(1) space complexity
