30277fc651ab306891e520e8104d2cb3a8d9ddf3
Mark P Thomas <markums@gmail.com>
2025-01-12 16:12:35 -0700

    Solved 2025

--- 0042_H_TrappingRainWater.js
+++ 0042_H_TrappingRainWater.js
@@ -1,5 +1,50 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 56:38 min
+// Patterns: Stack
 // Notes w.r.t. solution:
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let water = 0;
+  const stack = [];
+  for (let i = 1; i < height.length; i++) {
+    const currStep = height[i] - height[i - 1];
+    if (currStep < 0) {
+      // Drop, save max height & start
+      stack.push([i - 1, height[i - 1], height[i]]);
+    } else if (currStep > 0) {
+      // Step up, can fill if there is a matching drop
+
+      // Filled or unfilled max during this filling set
+      let maxBase = 0;
+      while (stack.length) {
+        const [prevIdx, prevTop, prevBase] = stack.pop();
+        const width = i - prevIdx - 1;
+
+        const currBase = height[i - 1];
+        maxBase = Math.max(maxBase, prevBase, currBase);
+
+        const currTop = height[i];
+        const fillHeight = Math.min(prevTop, currTop);
+        const depth = fillHeight - maxBase;
+
+        // Fill water & record prior level filled
+        water += width * depth;
+        maxBase = fillHeight;
+
+        if (prevTop > currTop) {
+          // Retain prior step for future potential fills
+          stack.push([prevIdx, prevTop, prevBase]);
+          break;
+        } else if (prevTop === currTop) {
+          break;
+        }
+      }
+    }
+  }
+
+  return water;
+};
--- 0297_H_SerializeDeserializeBinaryTree.js
+++ 0297_H_SerializeDeserializeBinaryTree.js
@@ -1,17 +1,94 @@
-// O() time complexity
-// O(1) space complexity
+// 2025/01/12
+// O(n) time complexity
+// O(n) space complexity
 // Time to complete: xx min
-// Patterns:
+// Patterns: BFS
 // Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+  const output = [];
+  if (!root) {
+    return '';
   }
-);
+
+  let row = [root]
+  while (row.length) {
+    const nextRow = [];
+    for (let i = 0; i < row.length; i++) {
+      if (row[i]) {
+        output.push(row[i].val);
+        nextRow.push(row[i].left)
+        nextRow.push(row[i].right)
+      } else {
+        output.push(null);
+      }
+    }
+    row = nextRow;
+  }
+
+  return output.join(',');
+};
+
+/**
+* Decodes your encoded data to tree.
+*
+* @param {string} data
+* @return {TreeNode}
+*/
+var deserialize = function (data) {
+  const array = data.split(',');
+  if (array[0] === '') {
+    return null;
+  }
+
+  const root = new TreeNode(Number(array[0]));
+
+  let row = [root];
+  let i = 0;
+  while (i < array.length && row.length) {
+    const nextRow = [];
+    for (let j = 0; j < row.length; j++) {
+      const parent = row[j];
+
+      i++;
+      console.log('i', i)
+      console.log('array[i]', array[i])
+      const nodeL = array[i] !== '' ? new TreeNode(Number(array[i])) : null;
+      if (nodeL) {
+        parent.left = nodeL;
+        nextRow.push(nodeL);
+      }
+
+      i++;
+      console.log('i', i)
+      console.log('array[i]', array[i])
+      const nodeR = array[i] !== '' ? new TreeNode(Number(array[i])) : null;
+      if (nodeR) {
+        parent.right = nodeR;
+        nextRow.push(nodeR);
+      }
+    }
+
+    row = nextRow;
+  }
+
+  return root;
+};
+
+/**
+* Your functions will be called as such:
+* deserialize(serialize(root));
+*/
