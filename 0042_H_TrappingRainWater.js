a426a0cc463add0c06ff6eef7dc4bc03bb5510bf
Mark P Thomas <markums@gmail.com>
2025-01-14 23:05:49 -0700

    Added worked solutions

--- 0042_H_TrappingRainWater.js
+++ 0042_H_TrappingRainWater.js
@@ -1,4 +1,71 @@
 // O(n) time complexity
+// O(1) space complexity
+// Time to complete: NA min
+// Patterns: 2 Pointer
+// Notes w.r.t. solution: LeetCode solution worked
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let water = 0;
+
+  let leftMax = 0;
+  let rightMax = 0;
+
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      leftMax = Math.max(leftMax, height[left]);
+      water += leftMax - height[left];
+      left++;
+    } else {
+      rightMax = Math.max(rightMax, height[right]);
+      water += rightMax - height[right];
+      right--;
+    }
+  }
+
+  return water;
+};
+
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: NA min
+// Patterns: Stack
+// Notes w.r.t. solution: LeetCode solution is simpler than mine
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let water = 0;
+  let next = 0;
+  const passed = [];
+  while (next < height.length) {
+    while (passed.length && height[next] > height[passed[passed.length - 1]]) {
+      const curr = passed.pop();
+      if (!passed.length) {
+        break;
+      }
+
+      const prev = passed[passed.length - 1];
+      const distance = next - prev - 1;
+      const boundedHeight = Math.min(height[next], height[prev]) - height[curr];
+      water += distance * boundedHeight;
+    }
+
+    passed.push(next);
+    next++;
+  }
+
+  return water;
+};
+
+
+// O(n) time complexity
 // O(n) space complexity
 // Time to complete: 56:38 min
 // Patterns: Stack
