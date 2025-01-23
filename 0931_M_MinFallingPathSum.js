0ad0227bdfc59e69ce80da3439beeb56d703a58b
Mark P Thomas <markums@gmail.com>
2025-01-22 22:08:05 -0700

    Added refactored solution to state reduction case.

--- 0931_M_MinFallingPathSum.js
+++ 0931_M_MinFallingPathSum.js
@@ -1,6 +1,46 @@
 // 2025/01/22
 // O(m * n) time complexity
 // O(m * n) space complexity
+// Time to complete: NA min
+// Patterns: Dynamic Programming - Matrix, State Reduction
+// Notes w.r.t. solution: Refactored from solution below
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var minFallingPathSum = function (matrix) {
+  let currMinSums = Array(matrix.length);
+
+  // Set base cases, which are the initial row
+  for (let col = 0; col < matrix[0].length; col++) {
+    currMinSums[col] = matrix[0][col];
+  }
+
+  // Process row-by-row
+  for (let row = 1; row < matrix.length; row++) {
+    const prevMinSums = [...currMinSums];
+    for (let col = 0; col < matrix[0].length; col++) {
+      const minSum = Math.min(
+        prevMinSums[col],
+        prevMinSums[col - 1] ?? Infinity,  // Check left diagonal
+        prevMinSums[col + 1] ?? Infinity); // Check right diagonal
+
+      currMinSums[col] = matrix[row][col] + minSum;
+    }
+  }
+
+  // Get min value from bottom row
+  let minSum = Infinity;
+  for (let col = 0; col < matrix[0].length; col++) {
+    minSum = Math.min(minSum, currMinSums[col]);
+  }
+
+  return minSum;
+};
+
+// 2025/01/22
+// O(m * n) time complexity
+// O(m * n) space complexity
 // Time to complete: 13:55 min
 // Patterns: Dynamic Programming - Matrix
 // Notes w.r.t. solution:
