82024644b21ee859144866f816fb888b34088849
Mark P Thomas <markums@gmail.com>
2025-01-25 16:11:27 -0700

    Solved 2025

--- 0025_H_ReferseNodesInKGroups.js
+++ 0025_H_ReferseNodesInKGroups.js
@@ -1,5 +1,55 @@
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 32:12 min
+// Patterns: Linked List - In-Place Reversal
 // Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+  if (k === 1) {
+    return head;
+  }
+
+  function reverse(prevTail, nextHead) {
+    let prev = null;
+    let node = prevTail.next;
+    while (node !== nextHead) {
+      const swap = node.next;
+      node.next = prev;
+      prev = node;
+      node = swap;
+    }
+
+    return [prev, prevTail.next];
+  }
+
+  const sentinel = new ListNode(null, head);
+  let prevTail = sentinel;
+  let nextHead = head;
+
+  let count = 0;
+  while (nextHead) {
+    count++;
+    nextHead = nextHead.next;
+
+    if (count % k === 0) {
+      const [revHead, revTail] = reverse(prevTail, nextHead);
+      prevTail.next = revHead;
+      revTail.next = nextHead;
+
+      prevTail = revTail;
+    }
+  }
+
+  return sentinel.next;
+};
