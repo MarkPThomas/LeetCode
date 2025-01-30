64ab145edcd314d30deb4852eabb3d870d5b5514
Mark P Thomas <markums@gmail.com>
2025-01-30 16:26:38 -0700

    Solved 2025

--- 1448_M_CountGoodNodesInBinaryTree.js
+++ 1448_M_CountGoodNodesInBinaryTree.js
@@ -1,6 +1,79 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 8:18 min / 10:35 min after refactor to stack
+// Patterns: Tree DFS
 // Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+  // node val >= prev nodes & >= root
+  // pass prev value to children to assess good
+  // if not good, pass along prev value, else update to curr
 
+  let count = 0;
+  const stack = [[root, root.val]];
+  while (stack.length) {
+    let [node, prevX] = stack.pop();
+
+    if (!node) {
+      continue;
+    }
+
+    if (prevX <= node.val) {
+      count++;
+      prevX = Math.max(prevX, node.val);
+    }
+
+    stack.push([node.left, prevX]);
+    stack.push([node.right, prevX]);
+  }
+
+  return count;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+  // node val >= prev nodes & >= root
+  // pass prev value to children to assess good
+  // if not good, pass along prev value, else update to curr
+
+  let count = 0;
+  function getGoodNodes(node, prevX) {
+    if (!node) {
+      return;
+    }
+
+    if (prevX <= node.val) {
+      count++;
+      prevX = Math.max(prevX, node.val);
+    }
+
+    getGoodNodes(node.left, prevX);
+    getGoodNodes(node.right, prevX);
+  }
+
+  getGoodNodes(root, root.val);
+
+  return count;
+};
--- 1456_M_MaxNumVowelsInSubstring.js
+++ 1456_M_MaxNumVowelsInSubstring.js
@@ -1,6 +1,35 @@
-// O() time complexity
+// 2025/01/30
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: 12:58 min
+// Patterns: Sliding Window
+// Notes w.r.t. solution: Would have been 8:45 or faster, but accidentally solved the wrong problem at first!
+//    Originally returned max # of any particular vowel in the window. :-P
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+  const vowels = new Set();
+  vowels.add('a');
+  vowels.add('e');
+  vowels.add('i');
+  vowels.add('o');
+  vowels.add('u');
 
+  let maxCount = 0;
+  let currCount = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (i >= k && vowels.has(s[i - k])) {
+      currCount--;
+    }
+
+    if (vowels.has(s[i])) {
+      currCount++;
+      maxCount = Math.max(maxCount, currCount);
+    }
+  }
+
+  return maxCount;
+};
