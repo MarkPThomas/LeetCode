bcc0ccc0b8ed9bba2cc4ab81bded45541be5d61d
Mark P Thomas <markums@gmail.com>
2025-01-31 10:04:49 -0700

    Solved 2025

--- 0220_H_ContainsDuplicateIII.js
+++ 0220_H_ContainsDuplicateIII.js
@@ -1,3 +1,41 @@
+// 2025/01/31
+// O(n) time complexity
+// O(min(n, k)) space complexity
+//  where n = # of numbers, k = index diff (window width)
+// Time to complete: 6:17 min
+// Patterns: Hashmap, Bucket Sort
+// Notes w.r.t. solution: Helped by remembering what to do.
+/**
+ * @param {number[]} nums
+ * @param {number} indexDiff
+ * @param {number} valueDiff
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function (nums, indexDiff, valueDiff) {
+  const buckets = {};
+  const w = valueDiff + 1;
+  const getId = (num) => Math.floor(num / w);
+
+  for (let i = 0; i < nums.length; i++) {
+    const enterId = getId(nums[i]);
+    // Check intersection
+    if (enterId in buckets
+      || ((enterId + 1) in buckets && Math.abs(nums[i] - buckets[enterId + 1]) < w)
+      || ((enterId - 1) in buckets && Math.abs(nums[i] - buckets[enterId - 1]) < w)) {
+      return true
+    }
+
+    // Update window
+    buckets[enterId] = nums[i];
+    if (i >= indexDiff) {
+      const leaveId = getId(nums[i - indexDiff]);
+      delete buckets[leaveId];
+    }
+  }
+
+  return false;
+};
+
 // 2025/01/29
 // O(n) time complexity
 // O(min(n, k)) space complexity
