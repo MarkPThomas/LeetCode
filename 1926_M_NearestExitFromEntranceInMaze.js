d5776562dd2965fc299d0ee6ee39e388d31cc5b7
Mark P Thomas <markums@gmail.com>
2025-02-06 22:49:20 -0700

    Solved 2025

--- 1926_M_NearestExitFromEntranceInMaze.js
+++ 1926_M_NearestExitFromEntranceInMaze.js
@@ -1,6 +1,45 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/02/06
+// O(m * n) time complexity
+// O(m * n) space complexity
+// Time to complete: 18:14 min
+// Patterns: BFS
 // Notes w.r.t. solution:
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+var nearestExit = function (maze, entrance) {
+  const WALL = '+';
+  const DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
+  const visited = {};
+  const queue = new Queue();
+  queue.enqueue([entrance, 0]);
+
+  while (queue.size()) {
+    ;
+    const [[row, col], dist] = queue.dequeue();
+    visited[[row, col]] = true;
+
+    for (const [deltaRow, deltaCol] of DIRS) {
+      const nextRow = row + deltaRow;
+      const nextCol = col + deltaCol;
+
+      if ([nextRow, nextCol] in visited) {
+        continue;
+      }
+
+      // Skip OOB
+      if (!maze[nextRow] || !maze[nextRow][nextCol]) {
+        if (dist) {
+          return dist;
+        }
+      } else if (maze[nextRow][nextCol] !== WALL) {
+        queue.enqueue([[nextRow, nextCol], dist + 1]);
+      }
+    }
+  }
+
+  return -1;
+};
