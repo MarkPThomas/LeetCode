fcbe098817f2708285648f2cf3b5ec966ef44935
Mark P Thomas <markums@gmail.com>
2025-02-11 16:50:57 -0700

    Solved 2025

--- 2352_M_EqualRowColPairs.js
+++ 2352_M_EqualRowColPairs.js
@@ -1,6 +1,38 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/02/11
+// O(n^2) time complexity
+// O(n^2) space complexity
+//  where n = # rows, # cols
+// Time to complete: 7:20 min
+// Patterns: Hashmap
 // Notes w.r.t. solution:
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function (grid) {
+  const rows = {};
+  for (let row = 0; row < grid.length; row++) {
+    const keys = [];
+    for (let col = 0; col < grid[0].length; col++) {
+      keys.push(grid[row][col]);
+    }
+    const key = keys.join('-');
+    rows[key] ??= [];
+    rows[key].push(row);
+  }
 
+  let numPairs = 0;
+  for (let col = 0; col < grid[0].length; col++) {
+    const keys = [];
+    for (let row = 0; row < grid.length; row++) {
+      keys.push(grid[row][col]);
+    }
+    const key = keys.join('-');
+
+    if (key in rows) {
+      numPairs += rows[key].length;
+    }
+  }
+
+  return numPairs;
+};
