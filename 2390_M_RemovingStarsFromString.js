fc49f63b87e8118c16d137886f9a8a3933f776b6
Mark P Thomas <markums@gmail.com>
2025-02-12 09:07:00 -0700

    Solved 2025

--- 2390_M_RemovingStarsFromString.js
+++ 2390_M_RemovingStarsFromString.js
@@ -1,6 +1,26 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/02/12
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 3:03 min
+// Patterns: Stack
 // Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  // build up stack of chars until a star is encountered
+  // remove the last char & skip the star
+  // repeat
 
+  const stack = [];
+  for (const char of s) {
+    if (char === '*') {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join('');
+};
