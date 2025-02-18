b87f74d4159a493bb29c17278b9e257e39d4a30f
Mark P Thomas <markums@gmail.com>
2025-02-18 09:37:16 -0700

    Refactored solution

--- 0045_M_JumpGameII.js
+++ 0045_M_JumpGameII.js
@@ -1,6 +1,29 @@
-// O() time complexity
+// 2025/02/18
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: NA min
+// Patterns: Greedy
+// Notes w.r.t. solution: Refactored JumpGame for notes. Not timed or from scratch.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  let farthest = 0;
+  let nextFarthest = 0;
+  let jumps = -1;
+  for (let i = 0; i <= farthest; i++) {
+    nextFarthest = Math.max(nextFarthest, i + nums[i]);
 
+    if (farthest === i) {
+      jumps++;
+
+      if (farthest >= nums.length - 1) {
+        return jumps;
+      }
+      farthest = nextFarthest;
+    }
+  }
+
+  return -1;
+};
