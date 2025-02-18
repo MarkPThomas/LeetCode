0577427864ca176529aed694884a7698a24edf11
Mark P Thomas <markums@gmail.com>
2025-02-18 08:54:27 -0700

    Solved 2025

--- 0055_M_JumpGame.js
+++ 0055_M_JumpGame.js
@@ -1,17 +1,24 @@
-// O() time complexity
+// 2025/02/18
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
-
-const testCases = [
-{ input: '',
-  expected: ''},
-];
-
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+// Time to complete: 30:57 min
+// Patterns: Greedy
+// Notes w.r.t. solution: Spent first 10 min trying to solve non-intuitive way that I remembered.
+// Would have been better off simulating w/ greedy in a more intuitive way.
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let farthest = 0;
+  for (let i = 0; i <= farthest; i++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (farthest >= nums.length - 1) {
+      return true;
+    } else if (farthest === i) {
+      return false;
+    }
   }
-);
+
+  return false;
+};
