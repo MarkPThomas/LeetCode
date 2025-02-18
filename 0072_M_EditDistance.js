a49b478e1a00b1326fa1bc2319a1acfaa1415a5f
Mark P Thomas <markums@gmail.com>
2025-02-17 22:50:10 -0700

    Added bottom-up worked solutions

--- 0072_M_EditDistance.js
+++ 0072_M_EditDistance.js
@@ -1,10 +1,108 @@
 // 2025/02/14
 // O(m * n) time complexity
-// O(m * n) -> O(min(m, n)) space complexity w/ state reduction
+// O(min(m, n)) space complexity w/ state reduction
 //  where m = length of word1, n = length of word2
 // Time to complete: NA min
 // Patterns: Dynamic Programming
-// Notes w.r.t. solution: Worked solution
+// Notes w.r.t. solution: Bottom-up w/ state reduction worked solution
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  if (word1.length === 0) {
+    return word2.length;
+  }
+
+  if (word2.length === 0) {
+    return word1.length;
+  }
+
+  const wordMin = word1.length <= word2.length ? word1 : word2;
+  const wordMax = word1.length > word2.length ? word1 : word2;
+
+  let dp = Array(wordMin.length + 1);
+  for (let idx = 0; idx <= wordMin.length; idx++) {
+    dp[idx] = idx;
+  }
+
+
+  for (let idxMax = 1; idxMax <= wordMax.length; idxMax++) {
+    const dpNext = [...dp];
+    dpNext[0]++;
+    for (let idxMin = 1; idxMin <= wordMin.length; idxMin++) {
+      if (wordMin[idxMin - 1] === wordMax[idxMax - 1]) {
+        dpNext[idxMin] = dp[idxMin - 1];
+      } else {
+        const insertChar = dp[idxMin];
+        const deleteChar = dpNext[idxMin - 1];
+        const replaceChar = dp[idxMin - 1];
+
+        dpNext[idxMin] = Math.min(insertChar, deleteChar, replaceChar) + 1;
+      }
+    }
+    dp = dpNext;
+  }
+
+  return dp[wordMin.length];
+};
+
+// 2025/02/14
+// O(m * n) time complexity
+// O(m * n) space complexity
+//  where m = length of word1, n = length of word2
+// Time to complete: NA min
+// Patterns: Dynamic Programming
+// Notes w.r.t. solution: Bottom-up worked solution
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  if (word1.length === 0) {
+    return word2.length;
+  }
+
+  if (word2.length === 0) {
+    return word1.length;
+  }
+
+  const dp = Array(word1.length + 1).fill().map(
+    () => Array(word2.length + 1));
+
+  for (let word1Idx = 0; word1Idx <= word1.length; word1Idx++) {
+    dp[word1Idx][0] = word1Idx;
+  }
+  for (let word2Idx = 0; word2Idx <= word2.length; word2Idx++) {
+    dp[0][word2Idx] = word2Idx;
+  }
+
+  for (let word1Idx = 1; word1Idx <= word1.length; word1Idx++) {
+    for (let word2Idx = 1; word2Idx <= word2.length; word2Idx++) {
+      if (word1[word1Idx - 1] === word2[word2Idx - 1]) {
+        dp[word1Idx][word2Idx] = dp[word1Idx - 1][word2Idx - 1];
+      } else {
+        const insertChar = dp[word1Idx][word2Idx - 1];
+        const deleteChar = dp[word1Idx - 1][word2Idx];
+        const replaceChar = dp[word1Idx - 1][word2Idx - 1];
+
+        dp[word1Idx][word2Idx] = Math.min(insertChar, deleteChar, replaceChar) + 1;
+      }
+    }
+  }
+
+  return dp[word1.length][word2.length];
+};
+
+// 2025/02/14
+// O(m * n) time complexity
+// O(m * n) space complexity
+//  where m = length of word1, n = length of word2
+// Time to complete: NA min
+// Patterns: Dynamic Programming
+// Notes w.r.t. solution: Top-down worked solution
 /**
  * @param {string} word1
  * @param {string} word2
