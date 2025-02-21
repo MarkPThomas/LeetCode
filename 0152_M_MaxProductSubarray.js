196acf1c1b27fd74afe5d1033fb00322f23d0d3c
Mark P Thomas <markums@gmail.com>
2025-02-21 15:35:02 -0700

    Solved 2025

--- 0152_M_MaxProductSubarray.js
+++ 0152_M_MaxProductSubarray.js
@@ -1,17 +1,38 @@
-// O() time complexity
+// 2025/02/21
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: 17:55 min
+// Patterns: Kadane's Algorithm
 // Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let max = -Infinity;
+  let currMaxNeg = null;
+  let currMaxNegRev = null;
+  let currMaxPos = null;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const numRev = nums[nums.length - 1 - i];
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+    // Assign if reset, else multiply
+    currMaxNeg = currMaxNeg ? currMaxNeg * num : num;
+    currMaxNegRev = currMaxNegRev ? currMaxNegRev * numRev : numRev;
+    if (num > 0) {
+      currMaxPos = currMaxPos ? currMaxPos * num : num;
+    }
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+    max = Math.max(max, currMaxNeg, currMaxNegRev);
+    if (currMaxPos) {
+      max = Math.max(max, currMaxPos);
+    }
+
+    if (num <= 0) { // Reset positive subarray if negative #
+      currMaxPos = null;
+    }
   }
-);
+
+  return max;
+};
