c72fc539967a1bc9a2490865d27ad44190f8c33c
Mark P Thomas <markums@gmail.com>
2025-02-21 15:40:59 -0700

    Worked solution

--- 0152_M_MaxProductSubarray.js
+++ 0152_M_MaxProductSubarray.js
@@ -1,6 +1,31 @@
 // 2025/02/21
 // O(n) time complexity
 // O(1) space complexity
+// Time to complete: NA min
+// Patterns: Kadane's Algorithm
+// Notes w.r.t. solution: Worked solution
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let max = -Infinity;
+  let currMax = 1;
+  let currMin = 1;
+  for (const num of nums) {
+    const tempMax = Math.max(num, currMax * num, currMin * num);
+    currMin = Math.min(num, currMax * num, currMin * num);
+    currMax = tempMax;
+
+    max = Math.max(max, currMax);
+  }
+
+  return max;
+};
+
+// 2025/02/21
+// O(n) time complexity
+// O(1) space complexity
 // Time to complete: 17:55 min
 // Patterns: Kadane's Algorithm
 // Notes w.r.t. solution:
