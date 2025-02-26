34c93e9c2f91b961e30af03e1e48030d736c8a65
Mark P Thomas <markums@gmail.com>
2025-02-26 09:28:20 -0700

    Solved 2025

--- 0271_M_EncodeDecodeStrings.js
+++ 0271_M_EncodeDecodeStrings.js
@@ -1,17 +1,64 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2025/02/26
+// O(n) time complexity
+// O(n * c) space complexity
+//  where c = length of delimiter, n = # words
+// Time to complete: 15:41 min
+// Patterns: String, Design
+// Notes w.r.t. solution: Consider escapes+delimiters to avoid false positives.
+//  Consider chunking (adding length of word w/ preceding delimiter) to add further validation
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function (strs) {
+  // str can be 0 length
+  // separator. 2xseparator indicates 0 length string.
+  const separator = '#!,!#';
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  return strs.join(separator);
+};
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+/**
+* Decodes a single string to a list of strings.
+*
+* @param {string} s
+* @return {string[]}
+*/
+var decode = function (s) {
+  const separator = '#!,!#';
+
+  function atSeparator(i, s, separator) {
+    let j = 0;
+    while (s[i] === separator[j] && j < separator.length) {
+      i++;
+      j++;
+    }
+
+    return j === separator.length;
   }
-);
+
+  const strs = [];
+  let str = [];
+  let i = 0;
+  while (i < s.length) {
+    const char = s[i];
+    if (atSeparator(i, s, separator)) {
+      strs.push(str.join(''));
+      str = [];
+      i += separator.length;
+    } else {
+      str.push(char);
+      i++;
+    }
+  }
+  strs.push(str.join(''));
+
+  return strs;
+};
+
+/**
+* Your functions will be called as such:
+* decode(encode(strs));
+*/
