6461063e647bde00f0e6258f82e0ef28d178112a
Mark P Thomas <markums@gmail.com>
2025-03-03 12:00:39 -0700

    Solved 2025

--- 0647_M_PalindromicSubstrings.js
+++ 0647_M_PalindromicSubstrings.js
@@ -1,17 +1,39 @@
-// O() time complexity
+// 2025/03/03
+// O(n^2) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: 8:15 min
+// Patterns: 2 Pointer
 // Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function (s) {
+  function countPalindromes(left, right, s) {
+    let numPals = 0;
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+    while (0 <= left && right < s.length) {
+      if (s[left] === s[right]) {
+        numPals++;
+        left--;
+        right++;
+      } else {
+        break;
+      }
+    }
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+    return numPals;
   }
-);
+
+  let numPals = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    // Check odd #
+    numPals += countPalindromes(i, i, s);
+
+    // Check even #
+    numPals += countPalindromes(i, i + 1, s);
+  }
+
+  return numPals;
+};
