20dc52cb4d7cfd834a518e7c184fc5b80754c5db
Mark P Thomas <markums@gmail.com>
2025-03-04 11:14:59 -0700

    Solved 2025

--- 0045_M_JumpGameII.js
+++ 0045_M_JumpGameII.js
@@ -1,3 +1,40 @@
+// 2025/03/04
+// O(n) time complexity
+// O(1) space complexity
+// Time to complete: 15:19 min
+// Patterns: Greedy
+// Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  if (nums.length === 1) {
+    return 0;
+  }
+
+  let numJumps = 0;
+  let range = 0;
+  let maxLastRange = range;
+  for (let i = 0; i < nums.length - 1; i++) {
+    maxLastRange = Math.max(maxLastRange, i + nums[i]);
+    if (!maxLastRange) { // We cannot jump anymore
+      return -1;
+    }
+
+    if (i === range) {
+      numJumps++;
+      range = maxLastRange;
+      if (range >= nums.length) { // We are able to reach the end
+        return numJumps;
+      }
+      maxLastRange = 0;
+    }
+  }
+
+  return numJumps;
+};
+
 // 2025/02/18
 // O(n) time complexity
 // O(1) space complexity
