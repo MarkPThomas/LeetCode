5d93861cbeaafc739a62f77a303d1e735ff8891c
Mark P Thomas <markums@gmail.com>
2025-03-04 11:29:26 -0700

    Solved 2025

--- 0124_H_BinaryTreeMaxPathSum.js
+++ 0124_H_BinaryTreeMaxPathSum.js
@@ -1,17 +1,40 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/03/04
+// O(n) time complexity
+// O(h) -> O(n) space complexity
+//  where n = # nodes, h = max depth of tree -> n
+// Time to complete: 7:24 min
+// Patterns: Tree DFS Postorder, Binary Tree
 // Notes w.r.t. solution:
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+  let maxSum = -Infinity;
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+  function dfs(node) {
+    if (!node) {
+      return 0;
+    }
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
+    const sumLeft = dfs(node.left);
+    const sumRight = dfs(node.right);
+
+    const sumChildren = sumLeft + sumRight + node.val;
+    const sumMaxBranch = Math.max(0, sumLeft, sumRight) + node.val;
+    maxSum = Math.max(maxSum, sumChildren, sumMaxBranch);
+
+    return sumMaxBranch;
   }
-);
+
+  dfs(root);
+  return maxSum;
+};
