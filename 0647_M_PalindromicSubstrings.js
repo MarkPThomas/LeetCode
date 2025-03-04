86a66c437c988a143a2e4f3c7ef841c891f80f7f
Mark P Thomas <markums@gmail.com>
2025-03-03 21:31:08 -0700

    Dynamic Programming solution

--- 0647_M_PalindromicSubstrings.js
+++ 0647_M_PalindromicSubstrings.js
@@ -1,10 +1,38 @@
 // 2025/03/03
 // O(n^2) time complexity
 // O(n^2) space complexity
-// Time to complete: xx min
+// Time to complete: 9:15 min
 // Patterns: Dynamic Programming
 // Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function (s) {
+  let numPals = 0;
 
+  const dp = Array(s.length).fill().map(() => Array(s.length).fill(false));
+  for (let i = 0; i < s.length; i++) {
+    dp[i][i] = true;
+    numPals++;
+    if (i < s.length - 1 && s[i] === s[i + 1]) {
+      dp[i][i + 1] = true;
+      numPals++;
+    }
+  }
+
+  for (let diff = 2; diff < s.length; diff++) {
+    for (let left = 0; left < s.length - diff; left++) {
+      const right = left + diff;
+      if (s[left] === s[right] && dp[left + 1][right - 1]) {
+        dp[left][right] = true;
+        numPals++;
+      }
+    }
+  }
+
+  return numPals;
+};
 
 // 2025/03/03
 // O(n^2) time complexity
