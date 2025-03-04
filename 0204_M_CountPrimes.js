8c2149a803accf430144060664ce0706fb62a7a6
Mark P Thomas <markums@gmail.com>
2025-03-04 12:11:01 -0700

    Updated w/ date/time & variable name change

--- 0204_M_CountPrimes.js
+++ 0204_M_CountPrimes.js
@@ -1,9 +1,9 @@
+// 2025/03/04
 // O(sqrt(n)*LogLog(n) + n) time complexity
 // O(n) space complexity
 // Time to complete: n/m min
 // Patterns: Greek math genius
 // Notes w.r.t. solution: This problem is dumb
-
 /**
  * @param {number} n
  * @return {number}
@@ -15,18 +15,18 @@ var countPrimes = function (n) {
 
   let p = 2;
   const limit = Math.sqrt(n);
-  const numbers = new Array(n);
+  const hasDivisors = new Array(n);
   for (p = 2; p <= limit; p++) {
-    if (!numbers[p]) {
+    if (!hasDivisors[p]) {
       for (j = p * p; j < n; j += p) {
-        numbers[j] = true;
+        hasDivisors[j] = true;
       }
     }
   }
 
   numberOfPrimes = 0;
   for (i = 2; i < n; i++) {
-    if (!numbers[i]) {
+    if (!hasDivisors[i]) {
       numberOfPrimes++;
     }
   }
