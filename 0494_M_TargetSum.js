68f07b6b9da0e832da213dfe7aa404dfa960647c
Mark P Thomas <markums@gmail.com>
2025-03-11 22:51:14 -0600

    Added bottom-up state reduction case.

--- 0494_M_TargetSum.js
+++ 0494_M_TargetSum.js
@@ -1,8 +1,44 @@
+// 2015/03/11
+// O(n * totalSum) time complexity
+// O(totalSum) space complexity
+// Time to complete: NA min
+// Patterns: Dynamic Programming - 0/1 Knapsack
+// Notes w.r.t. solution: Worked out solution w/ Bottom-Up w/ State Reduction
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function (nums, target) {
+  const totalSum = nums.reduce((acc, curr) => acc + curr);
+  if (Math.abs(target) > totalSum) {
+    return 0;
+  }
+
+  let dp = Array(2 * totalSum + 1).fill(0);
+  dp[totalSum + nums[0]] = 1;
+  dp[totalSum - nums[0]] += 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    const dpNext = Array(2 * totalSum + 1).fill(0);
+    for (let sum = -totalSum; sum <= totalSum; sum++) {
+      if (dp[sum + totalSum] > 0) {
+        dpNext[sum + totalSum + nums[i]] += dp[sum + totalSum];
+        dpNext[sum + totalSum - nums[i]] += dp[sum + totalSum];
+      }
+    }
+    dp = dpNext;
+  }
+
+  return dp[target + totalSum];
+};
+
+
 // 2015/01/10
 // O(n * totalSum) time complexity
 // O(n * totalSum) space complexity
 // Time to complete: NA min
-// Patterns: Dynamic Programming - Count
+// Patterns: Dynamic Programming - 0/1 Knapsack
 // Notes w.r.t. solution: Worked out solution w/ memoization
 /**
  * @param {number[]} nums
@@ -36,7 +72,7 @@ var findTargetSumWays = function (nums, target) {
 // O(2^n) time complexity
 // O(n) space complexity
 // Time to complete: 21:14 min
-// Patterns: Dynamic Programming - Count
+// Patterns: Dynamic Programming - 0/1 Knapsack
 // Notes w.r.t. solution:
 /**
  * @param {number[]} nums
