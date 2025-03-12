13e10b72619d8342b69d02a66461b14bba710340
Mark P Thomas <markums@gmail.com>
2025-03-12 16:31:17 -0600

    Solved 2025

--- 0148_M_SortList.js
+++ 0148_M_SortList.js
@@ -1,5 +1,65 @@
 // 2025/03/12
 // O(n * long(n)) time complexity
+// O(log(n)) space complexity
+// Time to complete: 11:30 min
+// Patterns: Linked List, Divide & Conquer, MergeSort
+// Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  const mid = getMid(head);
+  const left = sortList(head);
+  const right = sortList(mid);
+
+  return merge(left, right);
+
+  function getMid(head) {
+    let slow = null;
+    let fast = head;
+    while (fast && fast.next) {
+      slow = !slow ? head : slow.next;
+      fast = fast.next.next;
+    }
+    const mid = slow.next;
+    slow.next = null;
+
+    return mid;
+  }
+
+  function merge(list1, list2) {
+    const sentinel = new ListNode();
+    let node = sentinel;
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        node.next = list1;
+        list1 = list1.next;
+      } else {
+        node.next = list2;
+        list2 = list2.next;
+      }
+      node = node.next;
+    }
+    node.next = list1 ? list1 : list2;
+
+    return sentinel.next;
+  }
+};
+
+// 2025/03/12
+// O(n * long(n)) time complexity
 // O(n) space complexity
 // Time to complete: 8:50 min
 // Patterns: Linked List, PQ, Sorting
