caf1591b7b44696ae1d479041573713ee0745326
Mark P Thomas <markums@gmail.com>
2025-03-19 08:36:27 -0600

    Solved 2025

--- 0071_M_SimplifyPath.js
+++ 0071_M_SimplifyPath.js
@@ -1,6 +1,27 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/03/19
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 7:06 min
+// Patterns: Stack
 // Notes w.r.t. solution:
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+  const stack = [];
 
+  const pathElements = path.split('/');
+  for (const element of pathElements) {
+    if (element === '..') {
+      // move one directory up
+      stack.pop();
+    } else if (element === '' || element === '.') {
+      continue;
+    } else {
+      stack.push(element);
+    }
+  }
+
+  return '/' + stack.join('/');
+};
