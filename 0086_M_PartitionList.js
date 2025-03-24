ae5f7bf4a45c277f9a5c5fc6fe287d731d2ffa9d
Mark P Thomas <markums@gmail.com>
2025-03-24 07:53:17 -0600

    Solved 2025

--- 0086_M_PartitionList.js
+++ 0086_M_PartitionList.js
@@ -1,6 +1,43 @@
-// O() time complexity
+// 2025/03/23
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: 6:50 min
+// Patterns: Linked List - Two Pointer
 // Notes w.r.t. solution:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  const sentinelPre = new ListNode();
+  const sentinelPost = new ListNode();
 
+  let node = head;
+  let pre = sentinelPre;
+  let post = sentinelPost;
+  while (node) {
+    // add node to corresponding sentinel
+    if (node.val < x) {
+      pre.next = node;
+      pre = pre.next;
+    } else {
+      post.next = node;
+      post = post.next;
+    }
+    node = node.next;
+  }
+  post.next = null;
+
+  // Join last of pre to first of post
+  pre.next = sentinelPost.next;
+
+  return sentinelPre.next;
+};
