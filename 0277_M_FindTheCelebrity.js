c72fdeff5454346c3be13ae2cc4a097cea5729a8
Mark P Thomas <markums@gmail.com>
2025-03-27 16:23:35 -0600

    Solved 2025

--- 0277_M_FindTheCelebrity.js
+++ 0277_M_FindTheCelebrity.js
@@ -1,17 +1,57 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/03/27
+// O(n^2) time complexity
+// O(n) space complexity
+// Time to complete: 20:08 min
+// Patterns: Graph
 // Notes w.r.t. solution:
+/**
+ * Definition for knows()
+ *
+ * @param {integer} person a
+ * @param {integer} person b
+ * @return {boolean} whether a knows b
+ * knows = function(a, b) {
+ *     ...
+ * };
+ */
 
-const testCases = [
-{ input: '',
-  expected: ''},
-];
+/**
+ * @param {function} knows()
+ * @return {function}
+ */
+var solution = function (knows) {
+  /**
+   * @param {integer} n Total people
+   * @return {integer} The celebrity
+   */
+  return function (n) {
+    // DFS, check if cycle. Ignore cycle branches
+    // Count visited nodes, should result in n - 1;
 
-testCases.forEach((testCase) => {
-  // let result = FUT(testCase.input); // insert function name here
-  let pass = result === testCase.expected;
-  console.log(`Input: ${testCase.input}\nExpected: ${testCase.expected}\nResult: ${result}\nPass: ${pass}\n`);
-  }
-);
+    const inVisited = {};
+    const outVisited = {};
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (j === i) {
+          continue;
+        }
+
+        if (knows(i, j)) {
+          inVisited[j] ??= 0;
+          inVisited[j]++;
+
+          outVisited[i] ??= 0;
+          outVisited[i]++;
+        }
+      }
+    }
+
+    for (const [key, inDegrees] of Object.entries(inVisited)) {
+      if (inDegrees === n - 1 && !(key in outVisited)) {
+        return Number(key);
+      }
+    }
+
+    return -1;
+  };
+};
