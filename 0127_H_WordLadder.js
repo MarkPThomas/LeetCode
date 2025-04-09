357c679c0e3a2c5df0fc991327503864d653a62d
Mark P Thomas <markums@gmail.com>
2025-04-09 11:56:04 -0600

    Solved 2025

--- 0127_H_WordLadder.js
+++ 0127_H_WordLadder.js
@@ -1,6 +1,69 @@
-// yyyymmdd
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/04/06
+// O(n * c^2) time complexity
+// O(n * c) space complexity
+//  where n = # words in list, c = # chars in any word
+// Time to complete: 17:27 min
+// Patterns: Graph BFS
 // Notes w.r.t. solution:
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+  if (wordList.indexOf(endWord) === -1) {
+    return 0;
+  }
+
+  function getNextWords(prevWord, nextWords) {
+    for (const word of wordList) {
+      if (word in pastWords) {
+        continue;
+      }
+
+      if (has1CharDiff(prevWord, word)) {
+        nextWords.push(word);
+        pastWords[word] = true;
+      }
+    }
+  }
+
+  function has1CharDiff(word1, word2) {
+    let numDifferences = 0;
+    for (let i = 0; i < word1.length; i++) {
+      if (word1[i] !== word2[i]) {
+        numDifferences++;
+        if (numDifferences > 1) {
+          return false;
+        }
+      }
+    }
+    return !!numDifferences;
+  }
+
+  const pastWords = {};
+  pastWords[beginWord] = true;
+
+  let currWords = [];
+  getNextWords(beginWord, currWords);
+
+  let numMidWords = 0;
+  while (currWords.length) {
+    numMidWords++;
+
+    const nextWords = [];
+    for (let i = 0; i < currWords.length; i++) {
+      const word = currWords[i];
+
+      if (word === endWord) {
+        return numMidWords + 1;
+      }
+
+      getNextWords(word, nextWords);
+    }
+    currWords = nextWords;
+  }
+
+  return 0;
+};
--- 0433_M_MinGeneticMutation.js
+++ 0433_M_MinGeneticMutation.js
@@ -1,6 +1,72 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// O(n * c^2) time complexity
+// O(n * c) space complexity
+//  where n = # genes, c = # chars
+// Time to complete: 17:18 min
+// Patterns: Graph BFS
 // Notes w.r.t. solution:
+/**
+ * @param {string} startGene
+ * @param {string} endGene
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function (startGene, endGene, bank) {
+  // validate endGene is in bank
+  if (bank.indexOf(endGene) === -1) {
+    return -1;
+  }
 
+  // each gene can be representat as hash of A, C, G, T freqs
+  // can also note # of mutations different from startGene
+  //      order bankGenes by # mutations
+
+  // BFS or DFS of bank
+  // one gene mutation at a time?
+  function getNextMutations(prevGene, nextMutations) {
+    for (const gene of bank) {
+      if (gene in pastMutations) {
+        continue;
+      }
+
+      if (getNumMutations(prevGene, gene) === 1) {
+        nextMutations.push(gene);
+      }
+    }
+  }
+
+  function getNumMutations(gene1, gene2) {
+    let diffCount = 0;
+    for (let i = 0; i < gene1.length; i++) {
+      if (gene1[i] !== gene2[i]) {
+        diffCount++;
+      }
+    }
+    return diffCount;
+  }
+
+  const pastMutations = {};
+  pastMutations[startGene] = true;
+
+  let lastMutation = [];
+  getNextMutations(startGene, lastMutation); // T: n * g
+
+  let numMutations = 0;
+  while (lastMutation.length) {
+    numMutations++;
+
+    const nextMutation = [];
+    for (let i = 0; i < lastMutation.length; i++) {
+      const gene = lastMutation[i];
+      pastMutations[gene] = true;
+
+      if (gene === endGene) {
+        return numMutations;
+      }
+
+      getNextMutations(gene, nextMutation);
+    }
+    lastMutation = nextMutation;
+  }
+
+  return -1;
+};
