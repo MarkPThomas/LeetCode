624fee5dc95283e7fc31d8d465820d5ae3d915a2
Mark P Thomas <markums@gmail.com>
2025-05-04 09:54:09 -0600

    Clearing incorrect data

--- 0696_E_CountBinarySubstrings.js
+++ 0696_E_CountBinarySubstrings.js
@@ -1,43 +1,6 @@
-// 2025/05/04
-// O(n) time complexity
-// O(1) space complexity
-// Time to complete: 15:32 min
-// Patterns: Stack
+// yyyymmdd
+// O() time complexity
+// O() space complexity
+// Time to complete: xx min
+// Patterns:
 // Notes w.r.t. solution:
-/**
- * @param {string} s
- * @return {string}
- */
-var removeOuterParentheses = function (s) {
-  // decompose 's' into primitive sets
-  // combine result
-  const OPEN = '(';
-  const CLOSE = ')';
-
-  const result = [];
-
-  let inPrimitive = false;
-  let balance = 0;
-  for (const char of s) {
-    if (!inPrimitive) {
-      inPrimitive = true;
-    } else {
-      if (char === OPEN) {
-        balance--;
-      } else if (char === CLOSE) {
-        balance++;
-
-        if (balance > 0) {
-          balance = 0;
-          inPrimitive = false;
-        }
-      }
-
-      if (inPrimitive) {
-        result.push(char);
-      }
-    }
-  }
-
-  return result.join('');
-};
