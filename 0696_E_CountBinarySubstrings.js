a4fa20af7b8f00a6c7da6fed64e56afaad70b682
Mark P Thomas <markums@gmail.com>
2025-05-05 17:57:30 -0600

    Solved 2025

--- 0696_E_CountBinarySubstrings.js
+++ 0696_E_CountBinarySubstrings.js
@@ -1,6 +1,46 @@
-// yyyymmdd
-// O() time complexity
-// O() space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/05/05
+// O(n^2) time complexity
+// O(1) space complexity
+// Time to complete: 10:44 min
+// Patterns: 2 Pointer
 // Notes w.r.t. solution:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countBinarySubstrings = function (s) {
+  // equal # CONSECUTIVE 0s & 1s
+  // can start at each 01 & 10
+  //  from these, try growing outwards w/ same char
+  //      i.e. 01 -> 0011
+  //      Use 2 pointer one for 1/0, the other for 0/1
+  //      Iterate over string
+  //          Subloop of expand outwards
+
+  function addCount(left) {
+    let right = left + 1;
+
+    // Add initial count & increment once outwards to start expansion check
+    let count = 1;
+    left--;
+    right++;
+
+    while (0 <= left && s[left] === s[left + 1]
+      && right < s.length && s[right] === s[right - 1]) {
+      left--;
+      right++;
+      count++;
+    }
+
+    return count;
+  }
+
+  let count = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] !== s[i + 1]) {
+      count += addCount(i);
+    }
+  }
+
+  return count;
+};
