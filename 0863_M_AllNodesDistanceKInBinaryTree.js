d99709c4ce0c49b769e44621c7f6cfb40c044347
Mark P Thomas <markums@gmail.com>
2025-05-31 13:18:50 -0600

    Solved 2025

--- 0863_M_AllNodesDistanceKInBinaryTree.js
+++ 0863_M_AllNodesDistanceKInBinaryTree.js
@@ -1,3 +1,87 @@
+// 2025/05/31
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: OT 49:11 min
+// Patterns: DFS Postorder
+// Notes w.r.t. solution: Mostly solved in 30:00, 32/57 @ 42:05, 36/57 @ 45:47 debugging minor mistakes.
+//  Slow down a bit more before coding!
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function (root, target, k) {
+  // DFS to get target node & depth
+  // 1. Same DFS can continue k more calls to find child nodes
+  // 2. Same DFS, upon unwind, can search all roots <= k
+  //      Continued search is for k - delta_target->root nodes down unvisited branch
+  //      Better do use recursion rather than stack for post-DFS decision (DFS post-order)
+  // we need to track depth -> 0 @ target? Pass as param, return distance from target
+
+  const nodes = [];
+  function dfs(node, dist) {
+    if (!node) {
+      return dist;
+    }
+
+    if (dist !== undefined) {   // Target has been found
+      if (dist === k) {       // Save node & stop further calls
+        nodes.push(node.val);
+      } else if (dist < k) {  // Keep checking deeper
+        dfs(node.left, dist + 1);
+        dfs(node.right, dist + 1);
+      }
+
+      return Math.abs(dist - 1);  // Unwind distance
+    } else if (node === target) {  // Target found, start checking deeper
+      if (k === 0) {
+        nodes.push(node.val);
+        return 1;
+      }
+
+      // Check children & then unwind
+      dfs(node.left, 1);
+      dfs(node.right, 1);
+      dist = 0;
+    } else {    // Target not yet found
+      const distLeft = dfs(node.left);
+      if (distLeft !== undefined && distLeft <= k) { // Target in L branch
+        if (distLeft === k) {
+          nodes.push(node.val);
+        } else {    // Check remainder of right branch
+          dfs(node.right, distLeft + 1);
+        }
+
+        return Math.abs(distLeft + 1);  // Unwind distance
+      } else {                        // Look for target in right branch
+        const distRight = dfs(node.right);
+        if (distRight !== undefined && distRight <= k) {  // Target in R branch
+          if (distRight === k) {
+            nodes.push(node.val);
+          } else {    // Check remainder of left branch
+            dfs(node.left, distRight + 1);
+          }
+
+          return Math.abs(distRight + 1);  // Unwind distance
+        }
+      }
+    }
+
+    return dist === undefined ? dist : Math.abs(dist - 1);  // Unwind distance
+  }
+
+  dfs(root);
+  return nodes;
+};
+
 // 2025/04/22
 // O(n) time complexity
 // O(n) space complexity
