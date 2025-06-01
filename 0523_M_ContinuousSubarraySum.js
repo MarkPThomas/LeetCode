ed8f4ca2845e860a83aa494e1d7b9ee4021c5b31
Mark P Thomas <markums@gmail.com>
2025-06-01 17:00:12 -0600

    Attempted 2025

--- 0523_M_ContinuousSubarraySum.js
+++ 0523_M_ContinuousSubarraySum.js
@@ -1,7 +1,94 @@
-// yyyymmdd
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2025/06/01
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 18:48 min - 90/101 then OT
+// Patterns: Prefix Sum
+// Notes w.r.t. solution: Hashmap optimization
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function (nums, k) {
+  // count subarray sum n * k that has more than 2 elements
 
+  const sumFreq = {};
+  sumFreq[0] = true;
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+
+    if (nums[i] === 0 && nums[i + 1] === 0) {
+      return true;
+    }
+
+    if (sum && (sum - k) % k in sumFreq) {
+      if ((sum - k) % k === 0 && nums[i] !== sum && (sum - k) in sumFreq) {
+        return true;
+      }
+    }
+
+    sumFreq[sum] = true; // i
+  }
+
+  return false;
+};
+
+// 2025/06/01
+// O(n^2) time complexity
+// O(1) space complexity
+// Time to complete: 3:48 min - TLE @ 94/101
+// Patterns: Prefix Sum
+// Notes w.r.t. solution:
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function (nums, k) {
+  // count subarray sum n * k that has more than 2 elements
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum % k === 0 && j > i) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+// ==== Solution ====
+// O(n) time complexity
+// O(n) space complexity
+// Patterns: Prefix Sum
+// Notes w.r.t. solution: Hashmap optimization
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function (nums, k) {
+  // count subarray sum n * k that has more than 2 elements
+
+  const modSeen = {};
+  modSeen[0] = -1;
+
+  let prefixMod = 0;
+  for (let i = 0; i < nums.length; i++) {
+    prefixMod = (prefixMod + nums[i]) % k;
+
+    if (prefixMod in modSeen) { // has sum multiple of k
+      if (i - modSeen[prefixMod] > 1) { // size > 1
+        return true;
+      }
+    } else {
+      modSeen[prefixMod] = i;
+    }
+  }
+
+  return false;
+};
--- 0721_M_AccountsMerge.js
+++ 0721_M_AccountsMerge.js
@@ -1,6 +1,139 @@
+// 2025/04/15
 // O() time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// Time to complete: OT 1:11:43 min @ 5/50
+// Patterns: Graph - DFS,
 // Notes w.r.t. solution:
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function (accounts) {
+  // accounts[i] = [name, email1, email2, ..., email_i]
 
+  // accounts[i] are merged w/ accounts[j] if they have any emails in common
+  // Multiple accounts may have the same name, yet are different accounts
+  // I assume any account w/ a matching email must also ahve the same name?
+
+  // Find all owners to each email
+  const emails = {};
+  const ownersSkip = new Set();
+  for (let i = 0; i < accounts.length; i++) {
+    const account = accounts[i];
+    for (let j = 1; j < account.length; j++) {
+      const email = account[j];
+      if (email in emails) {
+        ownersSkip.add(i);
+      } else {
+        emails[email] = new Set();
+      }
+      emails[email].add(i)
+    }
+  }
+
+  // Re-add emails while consolidating amount multiple account owners
+  //  Choose lower account idx as default core owner
+  const emailsByAccount = Array(accounts.length);
+  for (const [email, ownersSet] of Object.entries(emails)) {
+    const owners = [...ownersSet].sort((a, b) => a - b);
+
+    const owner = owners[0];
+    if (ownersSkip.has(owner)) { // Skip owners that will be merged
+      continue;
+    }
+    emailsByAccount[owner] ??= new Set();
+    emailsByAccount[owner].add(email);
+
+    if (owners.length > 1) { // Add emails from redundant owners
+      for (let i = 1; i < owners.length; i++) {
+        const account = accounts[owners[i]];
+        for (let j = 1; j < account.length; j++) {
+          const mergedEmail = account[j];
+          emailsByAccount[owner].add(mergedEmail);
+        }
+      }
+    }
+  }
+
+  // For each account that is not empty, fetch name & add to sorted set of emails
+  const mergedAccounts = [];
+  for (let i = 0; i < emailsByAccount.length; i++) {
+    if (!emailsByAccount[i]) {
+      continue;
+    }
+
+    // merge accounts w/ [name, emails(sorted)]
+    const name = accounts[i][0];
+    const emails = [...emailsByAccount[i]].sort();
+    const mergedAccount = [name, ...emails];
+    mergedAccounts.push(mergedAccount);
+  }
+
+  return mergedAccounts;
+};
+
+// === Solutions
+// O(n * k * log(n * k)) time complexity
+// O(n * k) space complexity
+//  where n = # accounts, k = max # emails of any account
+// Patterns: Graph - DFS
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function (accounts) {
+  const visited = {};
+  const adjacent = {};
+
+  function dfs(email, mergedAccount) {
+    visited[email] = true;
+    mergedAccount.push(email);
+
+    if (!(email in adjacent)) {
+      return;
+    }
+
+    for (const neighbor of adjacent[email]) {
+      if (!visited[neighbor]) {
+        dfs(neighbor, mergedAccount);
+      }
+    }
+  }
+
+  // Create connectivity of all email addresses
+  for (const account of accounts) {
+    const firstEmail = account[1];
+    adjacent[firstEmail] ??= [];
+
+    // Build bi-directional adjacency list between first email & all other emails in an account
+    for (let j = 2; j < account.length; j++) {
+      const email = account[j];
+      adjacent[firstEmail].push(email);
+
+      adjacent[email] ??= [];
+      adjacent[email].push(firstEmail);
+    }
+  }
+
+  const mergedAccounts = [];
+  for (const account of accounts) {
+    const name = account[0];
+    const firstEmail = account[1];
+
+    if (!visited[firstEmail]) {
+      const mergedAccount = [];
+
+      dfs(firstEmail, mergedAccount);
+      mergedAccount.sort();
+
+      mergedAccounts.push([name, ...mergedAccount]);
+    }
+  }
+
+  return mergedAccounts;
+};
+
+// O(n * k * log(n * k)) time complexity
+// O(n * k) space complexity
+//  where n = # accounts, k = max # emails of any account
+// Patterns: Graph - DSU
