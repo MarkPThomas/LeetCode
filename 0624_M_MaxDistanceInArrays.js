717241d274b83648873ec4c5da0ee43b1607082e
Mark P Thomas <markums@gmail.com>
2025-06-20 11:38:22 -0600

    Solved 2025

--- 0624_M_MaxDistanceInArrays.js
+++ 0624_M_MaxDistanceInArrays.js
@@ -1,6 +1,99 @@
-// O() time complexity
+// 2025/06/20
+// O(n * m) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+//  where n = # arrays, m = avg array length
+// Time to complete: 12:03 min
+// Patterns: Greedy
 // Notes w.r.t. solution:
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function (arrays) {
+  // subtract smallest first # from largest last #
+  // If these occur in the same array,
+  //  track 2 lowest & 2 highest, by array
+  //  return max of the cross-subtractions
+  let min = {
+    val: Infinity,
+    idx: -1
+  }
 
+  let min2nd = {
+    val: Infinity,
+    idx: -1
+  }
+
+  let max = {
+    val: -Infinity,
+    idx: -1
+  }
+
+  let max2nd = {
+    val: -Infinity,
+    idx: -1
+  }
+
+  for (let i = 0; i < arrays.length; i++) {
+    const first = arrays[i][0];
+    if (first < min.val) {
+      min2nd.val = min.val;
+      min2nd.idx = min.idx;
+
+      min.val = first;
+      min.idx = i;
+    } else if (first < min2nd.val) {
+      min2nd.val = first;
+      min2nd.idx = i;
+    }
+
+    const last = arrays[i][arrays[i].length - 1];
+    if (last > max.val) {
+      max2nd.val = max.val;
+      max2nd.idx = max.idx;
+
+      max.val = last;
+      max.idx = i;
+    } else if (last > max2nd.val) {
+      max2nd.val = last;
+      max2nd.idx = i;
+    }
+  }
+
+  if (min.idx !== max.idx) {
+    return max.val - min.val;
+  } else {
+    return Math.max(
+      max.val - min2nd.val,
+      max2nd.val - min.val
+    );
+  }
+};
+
+// ===== Solutions =====
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function (arrays) {
+  let maxDelta = -Infinity;
+  let minVal = Infinity;
+  let maxVal = -Infinity;
+  for (let i = 0; i < arrays.length; i++) {
+    const minValCurr = arrays[i][0];
+    const maxValCurr = arrays[i][arrays[i].length - 1];
+
+    if (minVal !== Infinity && maxVal !== -Infinity) {
+      maxDelta = Math.max(maxDelta,
+        Math.max(
+          Math.abs(maxValCurr - minVal),
+          Math.abs(maxVal - minValCurr)
+        ));
+    }
+
+    minVal = Math.min(minVal, minValCurr);
+    maxVal = Math.max(maxVal, maxValCurr);
+  }
+
+  return maxDelta;
+};
--- 1272_M_RemoveINterval.js
+++ 1272_M_RemoveINterval.js
@@ -1,6 +1,37 @@
-// O() time complexity
+// 2025/06/20
+// O(n) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: 28:48 min
+// Patterns: Merge Interval
+// Notes w.r.t. solution: Would have solved faster, but jumped into coding too quickly, also minor big on interval exclusion.
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} toBeRemoved
+ * @return {number[][]}
+ */
+var removeInterval = function (intervals, toBeRemoved) {
+  const intervalsUpdated = [];
 
+  for (const interval of intervals) {
+    if (toBeRemoved[1] <= interval[0] || interval[1] <= toBeRemoved[0]) {
+      // No overlap - save as-is
+      intervalsUpdated.push([...interval]);
+    } else { // Overlaps - creates 2 new intervals at most
+      // Segment before remove interval
+      let start1 = interval[0];
+      let end1 = toBeRemoved[0];
+      if (start1 < end1) {
+        intervalsUpdated.push([start1, end1]);
+      }
+
+      // Segment after remove interval
+      let start2 = toBeRemoved[1];
+      let end2 = interval[1];
+      if (start2 < end2) {
+        intervalsUpdated.push([start2, end2]);
+      }
+    }
+  }
+
+  return intervalsUpdated;
+};
