a65b711eab490b8e8a9a76c561034373574697a3
Mark P Thomas <markums@gmail.com>
2025-06-23 11:55:04 -0600

    Solved 2025

--- 0981_M_TimeBasedKVStore.js
+++ 0981_M_TimeBasedKVStore.js
@@ -1,7 +1,72 @@
-// yyyymmdd
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/06/23
+// Initialize
+//  O(1) time complexity
+//  O(1) space complexity
+// Set
+//  O(1) time complexity
+//  O(m * L) space complexity
+// Get
+//  O(n * (L * log(m)) time complexity
+//  O(1) space complexity
+//  where n = # get calls, m = # set calls, L = average key/value string length
+//  and being extra nitpicky of including hashmap key hash time of L
+// Time to complete: 13:24 min
+// Patterns: Binary Search
 // Notes w.r.t. solution:
 
+var TimeMap = function () {
+  this.keySets = {};
+};
+
+/**
+ * @param {string} key
+ * @param {string} value
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function (key, value, timestamp) {
+  // timestamp strictly increasing => sorted ascending
+  this.keySets[key] ??= [];
+  this.keySets[key].push([value, timestamp]);
+};
+
+/**
+ * @param {string} key
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function (key, timestamp) {
+  // largest timestamp <= timestamp
+  const values = this.keySets[key];
+  if (!values || !values.length || timestamp < values[0][1]) {
+    return '';
+  } else if (values.length === 1) {
+    return values[0][0];
+  } else if (values[values.length - 1][1] <= timestamp) {
+    return values[values.length - 1][0];
+  }
+
+  // sorted ascending, so binary search
+  let left = 0;
+  let right = values.length;
+  while (right - left > 1) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (values[mid][1] < timestamp) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+
+  right = values[right][1] === timestamp ? right : right - 1;
+
+  return values[right][0];
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
--- 1100_M_FindKLengthSubstringsWithNoRepeatedChars.js
+++ 1100_M_FindKLengthSubstringsWithNoRepeatedChars.js
@@ -1,6 +1,47 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2025/06/23
+// O(n) time complexity
+// O(k) space complexity
+// Time to complete: 25:13 min
+// Patterns: Sliding Window, Hashmap
+// Notes w.r.t. solution: Most time was taken up working out subtlety of optimized unique char counting. Slow down!
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var numKLenSubstrNoRepeats = function (s, k) {
+  // sliding window + hashmap
 
+  let numSub = 0;
+
+  const charFreqs = {};
+  let numUnique = 0;
+  for (let i = 0; i < s.length; i++) {
+    // Remove char
+    const charOut = s[i - k];
+    if (charOut) {
+      if (charFreqs[charOut] === 2) {
+        numUnique++;
+      } else if (charFreqs[charOut] === 1) {
+        numUnique--;
+      }
+      charFreqs[charOut]--;
+    }
+
+    // Add char
+    const charIn = s[i];
+    if (!charFreqs[charIn] || charFreqs[charIn] === 0) {
+      numUnique++;
+    } else if (charFreqs[charIn] === 1) {
+      numUnique--;
+    }
+    charFreqs[charIn] = (charFreqs[charIn] ?? 0) + 1;
+
+    // Evaluate window
+    if (numUnique === k) {
+      numSub++;
+    }
+  }
+
+  return numSub;
+};
