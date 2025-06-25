a130d505ba707e0a8879014924effb6b4c4dcdb2
Mark P Thomas <markums@gmail.com>
2025-06-25 10:56:41 -0600

    Solved 2025

--- 0540_M_SingleElementInSortedArray.js
+++ 0540_M_SingleElementInSortedArray.js
@@ -1,7 +1,37 @@
-// yyyymmdd
-// O() time complexity
+// 2025/06/25
+// O(log(n)) time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: 18:54 min
+// Patterns: Binary Search
+// Notes w.r.t. solution: Watch those edge cases! Cost time addressing them after coding.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  let low = 0;
+  let high = nums.length - 1;
 
+  // Check ends
+  if (nums[low] !== nums[low + 1]) {
+    return nums[low];
+  } else if (nums[high] !== nums[high - 1]) {
+    return nums[high]
+  }
+
+  while (high - low > 1) {
+    const mid = low + Math.floor((high - low) / 2);
+    const firstIdx = nums[mid] === nums[mid - 1] ? mid - 1 : mid;
+    if (firstIdx % 2) { // first occurrence of val is @ odd idx
+      high = mid; // look left
+    } else {
+      low = mid; // look right
+    }
+  }
+
+  const numLow = nums[low];
+  const numMid = nums[low + 1];
+  const numHigh = nums[high];
+
+  return numLow === numMid ? numHigh : numLow;
+};
--- 1236_M_WebCrawler.js
+++ 1236_M_WebCrawler.js
@@ -1,6 +1,56 @@
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
+// 2025/06/25
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 17:25 min
+// Patterns: Graph - DFS
 // Notes w.r.t. solution:
+/**
+ * // This is the HtmlParser's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function HtmlParser() {
+ *
+ *		@param {string} url
+ *     	@return {string[]}
+ *     	this.getUrls = function(url) {
+ *      	...
+ *     	};
+ * };
+ */
 
+/**
+ * @param {string} startUrl
+ * @param {HtmlParser} htmlParser
+ * @return {string[]}
+*/
+var crawl = function (startUrl, htmlParser) {
+  // urls w/ & w/o a trailing "/" are considered different
+  // return all urls beneath domain of startURL
+  function getDomain(url) {
+    const urlPieces = url.split('/');
+    return urlPieces[0] + '//' + urlPieces[2];
+  }
+
+  const domain = getDomain(startUrl);
+  const domainUrls = [];
+
+  const visited = new Set([startUrl]);
+  const urls = [startUrl];
+  while (urls.length) {
+    const url = urls.pop();
+    domainUrls.push(url);
+
+    const nextUrls = htmlParser.getUrls(url);
+    for (const nextUrl of nextUrls) {
+      if (visited.has(nextUrl)) {
+        continue;
+      }
+      visited.add(nextUrl);
+
+      if (domain === nextUrl.substring(0, domain.length)) {
+        urls.push(nextUrl);
+      }
+    }
+  }
+
+  return domainUrls;
+};
