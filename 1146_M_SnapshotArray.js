db2e196dd74aeb38f4239502b14ba72506e387eb
Mark P Thomas <markums@gmail.com>
2025-06-29 20:45:08 -0600

    Solved 2025

--- 1146_M_SnapshotArray.js
+++ 1146_M_SnapshotArray.js
@@ -1,7 +1,94 @@
-// yyyymmdd
-// O() time complexity
-// O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// 2025/06/29
+// Constructor
+//  O(n) time complexity
+//  O(n + s) space complexity
+// Set
+//  O(1) time complexity
+//  O(s) space complexity
+// Snap
+//  O(1) time complexity
+//  O(1) space complexity
+// Get
+//  O(log(s)) time complexity
+//  O(1) space complexity
+//  where n = # indices, c = # times set is called, s = # snapshots taken
+// Time to complete: OT min
+// Patterns: Binary Search
+// Notes w.r.t. solution: Bad problem.
+//  Wasn't clear about edge cases w/o trial & error.
+//  Then got TLE. Only overcame through subtleties of using array over hashmap.
+/**
+ * @param {number} length
+ */
+var SnapshotArray = function (length) {
+  this.snapshots = Array(length).fill().map(() => []);
+  this.snaps = 0;
+};
 
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+SnapshotArray.prototype.set = function (index, val) {
+  const history = this.snapshots[index];
+  if (history.length && history[history.length - 1].snapId === this.snaps) {
+    history[history.length - 1].val = val;
+  } else {
+    this.snapshots[index].push({ val, snapId: this.snaps });
+  }
+};
+
+/**
+ * @return {number}
+ */
+SnapshotArray.prototype.snap = function () {
+  this.snaps++;
+  return this.snaps - 1;
+};
+
+/**
+ * @param {number} index
+ * @param {number} snap_id
+ * @return {number}
+ */
+SnapshotArray.prototype.get = function (index, snap_id) {
+  const history = this.snapshots[index];
+
+  if (!history.length) {
+    return 0;
+  }
+
+  let min = 0;
+  let max = history.length - 1;
+
+  if (snap_id < history[0].snapId) {
+    return 0;
+  } else if (history[max].snapId <= snap_id) {
+    return history[max].val;
+  }
+
+  // get max floor snapId
+  while (max - min > 1) {
+    const mid = min + Math.floor((max - min) / 2);
+    const snapId = history[mid].snapId;
+
+    if (snapId <= snap_id) {
+      min = mid;
+    } else {
+      max = mid;
+    }
+  }
+
+  return history[max].snapId === snap_id
+    ? history[max].val
+    : history[min].val;
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * var obj = new SnapshotArray(length)
+ * obj.set(index,val)
+ * var param_2 = obj.snap()
+ * var param_3 = obj.get(index,snap_id)
+ */
--- 1538_M_GuessMajorityInHiddenArray.js
+++ 1538_M_GuessMajorityInHiddenArray.js
@@ -1,7 +1,72 @@
-// yyyymmdd
+// 2025/06/29
 // O() time complexity
 // O(1) space complexity
-// Time to complete: xx min
-// Patterns:
-// Notes w.r.t. solution:
+// Time to complete: OT 32:47 min
+// Patterns: Math
+// Notes w.r.t. solution: Never worked out enough of a solution to code :-(
 
+// ===== Solution =====
+// O(n) time complexity
+// O(1) space complexity
+// Patterns: Math
+// Notes w.r.t. solution: This problem is dumb
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function ArrayReader() {
+ *     // Compares 4 different elements in the array
+ *     // return 4 if the values of the 4 elements are the same (0 or 1).
+ *     // return 2 if three elements have a value equal to 0 and one element has value equal to 1 or vice versa.
+ *     // return 0 : if two element have a value equal to 0 and two elements have a value equal to 1.
+ *     @param {number} a, b, c, d
+ *     @return {number}
+ *     this.query = function(a, b, c, d) {
+ *         ...
+ *     };
+ *
+ *     // Returns the length of the array
+ *     @return {number}
+ *     this.length = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {ArrayReader} reader
+ * @return {number}
+ */
+var guessMajority = function (reader) {
+  let countEqual = 1;
+  let countDiffer = 0;
+  let indexDiffer = -1;
+
+  function countOccurrence(equal, i) {
+    if (equal) {
+      countEqual++;
+    } else {
+      countDiffer++;
+      indexDiffer = i;
+    }
+  }
+
+  const query0123 = reader.query(0, 1, 2, 3);
+  const query1234 = reader.query(1, 2, 3, 4);
+
+  countOccurrence(query0123 === query1234, 4);
+  countOccurrence(reader.query(0, 2, 3, 4) === query1234, 1);
+  countOccurrence(reader.query(0, 1, 3, 4) === query1234, 2);
+  countOccurrence(reader.query(0, 1, 2, 4) === query1234, 3);
+
+  for (let i = 5; i < reader.length(); i++) {
+    countOccurrence(reader.query(1, 2, 3, i) === query0123, i);
+  }
+
+  if (countEqual > countDiffer) {
+    return 0;
+  } else if (countDiffer > countEqual) {
+    return indexDiffer
+  } else {
+    return -1;
+  }
+};
