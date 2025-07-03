db9a31a6bc7c693bf908e2f4e60616fcf02a680c
Mark P Thomas <markums@gmail.com>
2025-07-03 17:16:58 -0600

    Renamed files

--- 0142_M_LinkedListCycle_II.js
+++ 0142_M_LinkedListCycle_II.js
@@ -81,3 +81,34 @@ var detectCycle = function (head) {
   }
   return fast;
 };
+
+// O(n) time complexity
+// O(n) space complexity
+// Time to complete: 15 min
+// Patterns: Hash Map
+// Notes w.r.t. solution: Hash map in JavaScript returns a match for any stored node if a cycle exists, and node values are not unique.
+// The closest approximation here can only be done by adding a new property to nodes as they are visited and checking for this property.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let node = head;
+  while (node) {
+    if (node.visited) {
+      return node;
+    }
+    node.visited = true;
+    node = node.next;
+  }
+  return null;
+};
