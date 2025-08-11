cf6e0b9b146eef9bf01afc02fb538c44f44f587c
Mark P Thomas <markums@gmail.com>
2025-08-11 11:55:37 -0600

    Added solutions

--- 1197_M_MinKnightMoves.js
+++ 1197_M_MinKnightMoves.js
@@ -174,3 +174,214 @@ var minKnightMoves = function (x, y) {
     steps++;
   }
 };
+
+// ===== Solutions =====
+// O((max(|x|, |y|))^2) time complexity
+// O((max(|x|, |y|))^2) space complexity
+// Patterns: Graph BFS
+// Notes w.r.t. solution:
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function (x, y) {
+  // row: col
+  // -2: -1/+1
+  // +2: -1/+1
+  // -1/+1: -2
+  // -1/+1: +2
+  // BFS from origin doing all of these moves, unvisited?
+  const DIRS = [
+    [-2, -1], [-2, 1], [2, -1], [2, 1],
+    [-1, -2], [-1, 2], [1, -2], [1, 2]
+  ];
+
+  function isInBounds(row, col) {
+    if (x < 0 && y < 0) { // --
+      return row <= 1 && col <= 1;
+    } else if (x < 0) {   // -+
+      return row <= 1 && col >= -1;
+    } else if (y < 0) {   // +-
+      return row >= -1 && col <= 1;
+    } else {              // ++
+      return row >= -1 && col >= -1;
+    }
+  }
+
+  const visited = {};
+  let steps = 0;
+  let moves = [[0, 0]];
+  while (moves.length) {
+    const nextMoves = [];
+    for (let i = 0; i < moves.length; i++) {
+      const move = moves[i];
+      if (move[0] === x && move[1] === y) {
+        return steps;
+      }
+
+      for (const dir of DIRS) {
+        const nextX = move[0] + dir[0];
+        const nextY = move[1] + dir[1];
+        const nextMove = [nextX, nextY];
+
+        if (!visited[nextMove] && isInBounds(...nextMove)) {
+          nextMoves.push(nextMove);
+          visited[nextMove] = true;
+        }
+      }
+    }
+    moves = nextMoves;
+    steps++;
+  }
+};
+
+// O((max(|x|, |y|))^2) time complexity
+// O((max(|x|, |y|))^2) space complexity
+// Patterns: Graph BFS (Bi-Directional)
+// Notes w.r.t. solution:
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function (x, y) {
+  const DIRS = [
+    [-2, -1], [-2, 1], [2, -1], [2, 1],
+    [-1, -2], [-1, 2], [1, -2], [1, 2]
+  ];
+
+  function isInBounds(xMove, yMove, xTarget, yTarget) {
+    if (xTarget < 0 && yTarget < 0) { // Quad III (--)
+      return xMove <= 1 && yMove <= 1;
+    } else if (xTarget < 0) {   // Quad II (-+)
+      return xMove <= 1 && yMove >= -1;
+    } else if (yTarget < 0) {   // Quad III (+-)
+      return xMove >= -1 && yMove <= 1;
+    } else {              // Quad I (++)
+      return xMove >= -1 && yMove >= -1;
+    }
+  }
+
+  function checkMoves(steps, moves, visited, visitedOther, coordTarget, coordOrigin) {
+    const nextMoves = [];
+    for (let i = 0; i < moves.length; i++) {
+      const [moveX, moveY] = moves[i];
+      if ([moveX, moveY] in visitedOther) {
+        return [steps + visitedOther[[moveX, moveY]], null];
+      }
+
+      for (const [deltaX, deltaY] of DIRS) {
+        const nextX = moveX + deltaX;
+        const nextY = moveY + deltaY;
+
+        const nextMove = [nextX, nextY];
+        const nextMoveLocal = [nextX - coordOrigin[0], nextY - coordOrigin[1]];
+
+        if (!visited[nextMove] && isInBounds(...nextMoveLocal, ...coordTarget)) {
+          nextMoves.push(nextMove);
+          visited[nextMove] = steps + 1;
+        }
+      }
+    }
+
+    return [null, nextMoves];
+  }
+
+  let movesOrigin = [[0, 0]];
+  const seenOrigin = {};
+  seenOrigin[[0, 0]] = 0;
+
+  let movesPiece = [[x, y]];
+  const seenPiece = {};
+  seenPiece[[x, y]] = 0;
+
+  let steps = 0;
+  while (movesOrigin.length && movesPiece.length) {
+    const [originSteps, movesOriginNext] = checkMoves(
+      steps, movesOrigin, seenOrigin, seenPiece, [x, y], [0, 0]
+    );
+    if (movesOriginNext) {
+      movesOrigin = movesOriginNext;
+    } else {
+      return originSteps;
+    }
+
+    const [pieceSteps, movesPieceNext] = checkMoves(
+      steps, movesPiece, seenPiece, seenOrigin, [-x, -y], [x, y]
+    );
+    if (movesPieceNext) {
+      movesPiece = movesPieceNext;
+    } else {
+      return pieceSteps;
+    }
+
+    steps++;
+  }
+};
+
+
+// O(|x| * |y|) time complexity
+// O(|x| * |y|) space complexity
+// Patterns: Graph DFS + DP
+// Notes w.r.t. solution:
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function (x, y) {
+  const memo = {};
+
+  function dfs(x, y) {
+    const key = `${x}, ${y}`;
+    if (key in memo) {
+      return memo[key];
+    }
+
+    if (x + y === 0) {
+      return 0;
+    } else if (x + y === 2) {
+      return 2;
+    } else {
+      const minMoves = 1 + Math.min(
+        dfs(Math.abs(x - 1), Math.abs(y - 2)),
+        dfs(Math.abs(x - 2), Math.abs(y - 1))
+      );
+
+      memo[key] = minMoves;
+      return minMoves;
+    }
+  }
+
+  return dfs(Math.abs(x), Math.abs(y));
+};
+
+// O(|x| + |y|) time complexity
+// O(|x| + |y|) space complexity
+// Patterns: Recursion
+// Notes w.r.t. solution:
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function (x, y) {
+  if (x === 0 && y === 0) {
+    return 0;
+  } else if (x < 0 || y < 0) {
+    return minKnightMoves(Math.abs(x), Math.abs(y));
+  } else if (y > x) {
+    return minKnightMoves(y, x);
+  } else if (
+    (x === 3 && y === 1)
+    || (x === 4 && y === 3)
+    || (x === 1 && y === 1)) {
+
+    return 1 + minKnightMoves(x - 2, y + 1);
+  } else if (x === 2 && y === 0) {
+    return 1 + minKnightMoves(x - 1, y + 2);
+  } else {
+    return 1 + minKnightMoves(x - 2, y - 1);
+  }
+};
