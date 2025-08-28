8d098ede3fb672477970dbeb116d4eb67089ba27
Mark P Thomas <markums@gmail.com>
2025-08-28 11:12:55 -0600

    Added solution

--- 0317_H_ShortestDistanceFromAllBuildings.js
+++ 0317_H_ShortestDistanceFromAllBuildings.js
@@ -96,3 +96,86 @@ var shortestDistance = function (grid) {
 
   return minDistance === Infinity ? -1 : minDistance;
 };
+
+// ===== Solutions =====
+// O(n^2 * m^2) time complexity
+// O(n * m) space complexity
+// Patterns: BFS
+const DIRS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+const EMPTY = 0;
+const HOUSE = 1;
+const OBSTACLE = 2;
+
+let bfs = function (grid, distances, row, col) {
+  const visited = new Array(grid.length).fill().map(
+    () => new Array(grid[0].length).fill(false));
+  visited[row][col] = true;
+
+  let cells = [[row, col]];
+  let steps = 0;
+  while (cells.length) {
+
+    const nextCells = [];
+    for (let i = 0; i < cells.length; i++) {
+      const [row, col] = cells[i];
+
+      if (grid[row][col] === EMPTY) {
+        distances[row][col].sumDist += steps;
+        distances[row][col].numHouses++;
+      }
+
+      for (const [rowDelt, colDelt] of DIRS) {
+        const nextRow = row + rowDelt;
+        const nextCol = col + colDelt;
+
+        if (isInBounds(nextRow, nextCol, grid)
+          && !visited[nextRow][nextCol]
+          && grid[nextRow][nextCol] === EMPTY) {
+
+          visited[nextRow][nextCol] = true;
+          nextCells.push([nextRow, nextCol]);
+        }
+
+      }
+    }
+
+    cells = nextCells;
+    steps++;
+  }
+};
+
+let isInBounds = function (row, col, grid) {
+  return 0 <= row && row < grid.length
+    && 0 <= col && col < grid[0].length;
+}
+
+let shortestDistance = function (grid) {
+  // Store { total_dist, houses_count } for each cell.
+  const distances = new Array(grid.length).fill().map(
+    () => new Array(grid[0].length).fill().map(
+      () => ({ numHouses: 0, sumDist: 0 }
+      )));
+
+  // Count houses and start BFS from each house.
+  let totalHouses = 0;
+  for (let row = 0; row < grid.length; ++row) {
+    for (let col = 0; col < grid[0].length; ++col) {
+      if (grid[row][col] === HOUSE) {
+        totalHouses++;
+        bfs(grid, distances, row, col);
+      }
+    }
+  }
+
+  // Check all empty lands with houses count equal to total houses & find the min ans.
+  let minDistance = Infinity;
+  for (let row = 0; row < grid.length; ++row) {
+    for (let col = 0; col < grid[0].length; ++col) {
+      if (distances[row][col].numHouses === totalHouses) {
+        minDistance = Math.min(minDistance, distances[row][col].sumDist);
+      }
+    }
+  }
+
+  return minDistance === Infinity ? -1 : minDistance;
+};
